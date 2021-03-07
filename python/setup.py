3a3706214777b93cac5fe5761cca2eb46ce8d4d5
Tom Hacohen <tom@stosb.com>
2021-03-07 21:51:16 +0200

    Python: bump version.

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ import os
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "diahook"
-VERSION = "0.8.4"
+VERSION = "0.8.5"
 # To install the library, run the following
 #
 # python setup.py install
