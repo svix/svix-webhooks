cee1b50748d7338c4a9122c3f21113b1e1ff2879
Tom Hacohen <tom@stosb.com>
2021-04-28 13:44:58 +0300

    Make set an array instead of an actual set.

--- javascript/src/openapi/models/Set.ts
+++ javascript/src/openapi/models/Set.ts
@@ -1,13 +1,1 @@
-export interface Set<T> {
-  add(value: T): Set<T>;
-  clear(): void;
-  delete(value: T): boolean;
-  entries(): IterableIterator<[T, T]>;
-  forEach(callbackfn: (value: T, index: T, set: Set<T>) => void, thisArg?: any): void;
-  has(value: T): boolean;
-  keys(): IterableIterator<T>;
-  size: number;
-  values(): IterableIterator<T>;
-  [Symbol.iterator]():IterableIterator<T>;
-  [Symbol.toStringTag]: string;
-}
+export const Set = Array;
