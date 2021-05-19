a656fcff0c8074ed0df435b0db34ad463b549fe2
Tom Hacohen <tom@stosb.com>
2021-05-19 13:32:14 +0300

    Fix formatting.

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -144,8 +144,16 @@ class Endpoint {
     return this.api.getEndpointApiV1AppAppIdEndpointEndpointIdGet({ endpointId, appId });
   }
 
-  public update(appId: string, endpointId: string, endpointIn: EndpointIn): Promise<EndpointOut> {
-    return this.api.updateEndpointApiV1AppAppIdEndpointEndpointIdPut({ appId, endpointId, endpointIn });
+  public update(
+    appId: string,
+    endpointId: string,
+    endpointIn: EndpointIn
+  ): Promise<EndpointOut> {
+    return this.api.updateEndpointApiV1AppAppIdEndpointEndpointIdPut({
+      appId,
+      endpointId,
+      endpointIn,
+    });
   }
 
   public delete(appId: string, endpointId: string): Promise<void> {
