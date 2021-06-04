9145f32e0aee14d339eb33b53ea25a470b349419
Frank Chiarulli Jr <frank@svix.com>
2021-06-03 22:24:06 -0400

    refactor to array of headers

--- php/lib/Webhook.php
+++ php/lib/Webhook.php
@@ -2,49 +2,49 @@
 
 namespace Svix;
 
+class WebhookVerificationException extends \Exception {
+    public function __construct($message, $code = 0, Throwable $previous = null) {
+            parent::__construct($message, $code, $previous);
+    }
+}
+
 class Webhook {
     public function __construct(string $secret) {
         $this->secret = base64_decode($secret);
     }
 
     public function verify($payload, $headers) {
-        $msgId = self::getHeaderByName("svix-id", $headers);
-        $timestamp = self::getHeaderByName("svix-timestamp", $headers);
-        $signatures = self::getHeaderByName("svix-signature", $headers);
+        if (!isset($headers['svix-id']) || !isset($headers['svix-timestamp']) || !isset($headers['svix-signature'])) {
+            throw new WebhookVerificationException("Missing required headers");
+        }
+
+        $msgId = $headers['svix-id'];
+        $timestamp = $headers['svix-timestamp'];
+        $msgSignature = $headers['svix-signature'];
 
         $toSign = "{$msgId}.{$timestamp}.{$payload}";
         $signature = self::sign($this->secret, $toSign);
 
-        
-        $passedSignatures = \explode(' ', $signature);
+        $passedSignatures = explode(' ', $msgSignature);
         foreach ($passedSignatures as $versionedSignature) {
-            $sigParts = \explode(',', $item, 2);
+            $sigParts = explode(',', $versionedSignature, 2);
             // TODO check len of array
             $version = $sigParts[0];
             $expectedSignature = $sigParts[1];
-            if ($version != "v1") {
+
+            if (strcmp($version, "v1") != 0) {
                 continue;
             }
-            if ($signature == $expectedSignature) {
-                return \json_decode($payload, true);
+
+            if (strcmp($signature, $expectedSignature) == 0) {
+                return json_decode($payload, true);
             }
         }
-        throw new Exception\WebhookVerificationException("No matching signature found");
-    }
-
-    private static function getHeaderByName($name, $headers) {
-        $items = \explode(',', $header);
-
-        foreach ($items as $item) {
-            $itemParts = \explode('=', $item, 2);
-            if ($name === $itemParts[0]) {
-                return $itemParts[1];
-            }
-        }
-        throw new Exception\WebhookVerificationException("Missing required headers");
+        throw new WebhookVerificationException("No matching signature found");
     }
 
     private static function sign($key, $payload) {
-        return base64_decode(\hash_hmac('sha256', $payload, $key));
+        $hex_hash = hash_hmac('sha256', $payload, $key);
+        return base64_encode(pack('H*',$hex_hash));
     }
 }
