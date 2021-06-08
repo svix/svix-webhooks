c7b9cc053ed9e7fb18faf3d0d4e60965477b74b4
Tal Hacohen <hacohen.tal@gmail.com>
2021-06-08 21:07:09 +0300

    Change Webhook to return boolean and add tests

--- java/src/main/java/svix/Webhook.java
+++ java/src/main/java/svix/Webhook.java
@@ -3,14 +3,19 @@ package svix;
 import java.net.http.HttpHeaders;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.List;
 import java.util.Optional;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Webhook {
-	private static final String HMAC_SHA512 = "HmacSHA512";
+	static final String MSG_ID_KEY = "svix-id";
+	static final String MSG_SIGNATURE_KEY = "svix-signature";
+	static final String MSG_TIMESTAMP_KEY = "svix-timestamp";
+	private static final String HMAC_SHA256 = "HmacSHA256";
 
 	private final byte[] key;
 
@@ -18,30 +23,29 @@ public class Webhook {
 		this.key = Base64.getDecoder().decode(secret);
 	}
 
-	public String verify(String payload, HttpHeaders headers) throws WebhookVerificationError {
-		Optional<String> msgId = headers.firstValue("svix-id");
-		Optional<String> msgSignature = headers.firstValue("svix-signature");
-		Optional<String> msgTimestamp = headers.firstValue("svix-timestamp");
+	public boolean verify(String payload, HttpHeaders headers) throws WebhookVerificationError {
+		Optional<String> msgId = headers.firstValue(MSG_ID_KEY);
+		List<String> msgSignature = headers.allValues(MSG_SIGNATURE_KEY);
+		Optional<String> msgTimestamp = headers.firstValue(MSG_TIMESTAMP_KEY);
 
 		if (msgId.isEmpty() || msgSignature.isEmpty() || msgTimestamp.isEmpty()) {
 			throw new WebhookVerificationError("Missing required headers");
 		}
 
-		String toSign = String.format("%s.%s.%s", msgId, msgSignature, msgTimestamp);
+		String toSign = String.format("%s.%s.%s", msgId.get(), msgTimestamp.get(), payload);
 		String expectedSignature = sign(toSign);
-		String[] passedSignatures = expectedSignature.split(" ");
-		for (String versionedSignature : passedSignatures) {
+		for (String versionedSignature : msgSignature) {
 			String[] sigParts = versionedSignature.split(",");
 			if (sigParts.length < 2) {
 				continue;
 			}
 			String version = sigParts[0];
-			String signature = sigParts[1];
 			if (!version.equals("v1")) {
 				continue;
 			}
-			if (signature.equals(expectedSignature)) {
-				return new JSON().serialize(payload);
+			String signature = sigParts[1];
+			if (MessageDigest.isEqual(signature.getBytes(), expectedSignature.getBytes())) {
+				return true;
 			}
 		}
 		throw new WebhookVerificationError("No matching signature found");
@@ -49,11 +53,10 @@ public class Webhook {
 
 	private String sign(String toSign) throws WebhookVerificationError {
 		try {
-			Mac sha512Hmac = Mac.getInstance(HMAC_SHA512);
-			SecretKeySpec keySpec = new SecretKeySpec(key, HMAC_SHA512);
+			Mac sha512Hmac = Mac.getInstance(HMAC_SHA256);
+			SecretKeySpec keySpec = new SecretKeySpec(key, HMAC_SHA256);
 			sha512Hmac.init(keySpec);
 			byte[] macData = sha512Hmac.doFinal(toSign.getBytes(StandardCharsets.UTF_8));
-
 			return Base64.getEncoder().encodeToString(macData);
 		} catch (InvalidKeyException | NoSuchAlgorithmException e) {
 			throw new WebhookVerificationError(e.getMessage());
