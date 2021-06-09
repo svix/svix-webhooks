da5690dd18e19533f397b79c3c63bd843052e9da
Frank Chiarulli Jr <frank@svix.com>
2021-06-09 16:49:13 -0400

    extend

--- java/lib/src/main/java/com/svix/exceptions/ApiException.java
+++ java/lib/src/main/java/com/svix/exceptions/ApiException.java
@@ -3,42 +3,8 @@ package com.svix.exceptions;
 import java.util.Map;
 import java.util.List;
 
-public class ApiException extends Exception {
-    private int code = 0;
-    private Map<String, List<String>> responseHeaders = null;
-    private String responseBody = null;
-
+public class ApiException extends com.svix.internal.ApiException {
     public ApiException(final String message, final Throwable throwable, final int code, final Map<String, List<String>> responseHeaders, final String responseBody) {
-        super(message, throwable);
-        this.code = code;
-        this.responseHeaders = responseHeaders;
-        this.responseBody = responseBody;
-    }
-
-    /**
-     * Get the HTTP status code.
-     *
-     * @return HTTP status code
-     */
-    public int getCode() {
-        return code;
-    }
-
-    /**
-     * Get the HTTP response headers.
-     *
-     * @return A map of list of string
-     */
-    public Map<String, List<String>> getResponseHeaders() {
-        return responseHeaders;
-    }
-
-    /**
-     * Get the HTTP response body.
-     *
-     * @return Response body in the form of string
-     */
-    public String getResponseBody() {
-        return responseBody;
+        super(message, throwable, code, responseHeaders, responseBody);
     }
 }
