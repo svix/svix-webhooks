41f3de43fa8ac596c57acc628f2d8dcbcdec3a83
Frank Chiarulli Jr <frank@svix.com>
2021-07-26 09:55:41 -0400

    Javascript: Implement Retries

--- javascript/src/openapi/http/isomorphic-fetch.ts
+++ javascript/src/openapi/http/isomorphic-fetch.ts
@@ -2,13 +2,36 @@ import {HttpLibrary, RequestContext, ResponseContext} from './http';
 import { from, Observable } from '../rxjsStub';
 import "isomorphic-fetch";
 
+const numRetries = 2;
+const sleep = (interval: number) => new Promise(resolve => setTimeout(resolve, interval));
+
 export class IsomorphicFetchHttpLibrary implements HttpLibrary {
 
     public send(request: RequestContext): Observable<ResponseContext> {
+      const resultPromise = this.sendWithRetry(request, numRetries, 50);
+      return from<Promise<ResponseContext>>(resultPromise);
+    }
+
+    private async sendWithRetry(request: RequestContext, triesLeft: number, nextInterval: number): Promise<ResponseContext> {
+      try {
+        const response = await this.sendOnce(request);
+        if (triesLeft <= 0 || response.httpStatusCode < 500) {
+          return response;
+        }
+      } catch (e) {
+        if (triesLeft <= 0) {
+          throw e;
+        }
+      };
+      await sleep(nextInterval);
+      return await this.sendWithRetry(request, --triesLeft, nextInterval * 2);
+    }
+  
+    private sendOnce(request: RequestContext): Promise<ResponseContext> {
         let method = request.getHttpMethod().toString();
         let body = request.getBody();
 
-        const resultPromise = fetch(request.getUrl(), {
+        return fetch(request.getUrl(), {
             method: method,
             body: body as any,
             headers: request.getHeaders(),
@@ -25,8 +48,5 @@ export class IsomorphicFetchHttpLibrary implements HttpLibrary {
             };
             return new ResponseContext(resp.status, headers, body);
         });
-
-        return from<Promise<ResponseContext>>(resultPromise);
-
     }
 }
