6c52ca786c8cfd095a07838bd46438f5835bb3bc
Frank Chiarulli Jr <frank@svix.com>
2021-07-26 09:55:26 -0400

    Go: Implement Retries

--- regen_openapi.sh
+++ regen_openapi.sh
@@ -10,6 +10,6 @@ yarn openapi-generator-cli generate -i openapi.json -g python -o python/ -c pyth
 
 yarn openapi-generator-cli generate -i openapi.json -g typescript -o javascript/src/openapi -c javascript/openapi-generator-config.json
 
-yarn openapi-generator-cli generate -i openapi.json -g go -o go/internal/openapi -c go/openapi-generator-config.json
+yarn openapi-generator-cli generate -i openapi.json -g go -o go/internal/openapi -c go/openapi-generator-config.json -t go/templates
 
 yarn openapi-generator-cli generate -i openapi.json -g java -o java/lib/generated/openapi -c java/openapi-generator-config.json
