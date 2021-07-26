f79e3d0a9b9bd1efd3954085b64f6a348f2db46a
Frank Chiarulli Jr <frank@svix.com>
2021-07-26 09:55:54 -0400

    Python: Implement Retries

--- regen_openapi.sh
+++ regen_openapi.sh
@@ -6,7 +6,7 @@ if [ -n "$1" ]; then
     curl "$1" | python -m json.tool > openapi.json
 fi
 
-yarn openapi-generator-cli generate -i openapi.json -g python -o python/ -c python/openapi-generator-config.json
+yarn openapi-generator-cli generate -i openapi.json -g python -o python/ -c python/openapi-generator-config.json -t python/templates
 
 yarn openapi-generator-cli generate -i openapi.json -g typescript -o javascript/src/openapi -c javascript/openapi-generator-config.json
 
