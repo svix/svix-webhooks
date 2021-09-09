be9d411e4f20367bd5ea3ec50224364f14786a54
Frank <84723448+svix-frank@users.noreply.github.com>
2021-09-09 15:03:31 -0400

    Java: APIException set error message to error detail and code if possible (#130)
    * Java: APIException try to use body if no message is found
    
    * Parse exception
    
    * Print code as well

--- java/lib/src/main/java/com/svix/exceptions/ApiException.java
+++ java/lib/src/main/java/com/svix/exceptions/ApiException.java
@@ -2,9 +2,44 @@ package com.svix.exceptions;
 
 import java.util.Map;
 import java.util.List;
+import com.google.gson.Gson;
+
+class APIError {
+    private String code;
+    private String detail;
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public String getDetail() {
+        return this.detail;
+    }
+}
 
 public class ApiException extends com.svix.internal.ApiException {
+
+    private String message;
+
     public ApiException(final String message, final Throwable throwable, final int code, final Map<String, List<String>> responseHeaders, final String responseBody) {
         super(message, throwable, code, responseHeaders, responseBody);
+
+        if ((message == null || message.isEmpty()) && responseBody != null) {
+            try {
+                Gson gson = new Gson();
+                APIError error = gson.fromJson(responseBody, APIError.class);
+                this.message = String.format("%s (%s)", error.getDetail(), error.getCode());
+            } catch (Exception e) {
+                this.message = responseBody;
+            }
+        }
+    }
+
+    public String getMessage() {
+        String msg = super.getMessage();
+        if (msg != null && !msg.isEmpty()) {
+            return msg;
+        }
+        return this.message;
     }
 }
