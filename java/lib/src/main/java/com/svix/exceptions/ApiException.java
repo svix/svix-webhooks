fdc593742f7dd0aa11d187e9483d9fb551a605c1
Frank Chiarulli Jr <frank@svix.com>
2021-09-09 10:28:29 -0400

    Java: APIException try to use body if no message is found

--- java/lib/src/main/java/com/svix/exceptions/ApiException.java
+++ java/lib/src/main/java/com/svix/exceptions/ApiException.java
@@ -7,4 +7,12 @@ public class ApiException extends com.svix.internal.ApiException {
     public ApiException(final String message, final Throwable throwable, final int code, final Map<String, List<String>> responseHeaders, final String responseBody) {
         super(message, throwable, code, responseHeaders, responseBody);
     }
+
+    public String getMessage() {
+        String msg = super.getMessage();
+        if (msg == "") {
+            msg = this.getResponseBody();
+        }
+        return msg;
+    }
 }
