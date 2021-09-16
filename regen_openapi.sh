810a8ba35b7689c0eea791e2eb1e6fc7f03f2573
Frank Chiarulli Jr <frank@svix.com>
2021-09-16 11:52:03 -0400

    initial project structure

--- regen_openapi.sh
+++ regen_openapi.sh
@@ -13,3 +13,5 @@ yarn openapi-generator-cli generate -i openapi.json -g typescript -o javascript/
 yarn openapi-generator-cli generate -i openapi.json -g go -o go/internal/openapi -c go/openapi-generator-config.json -t go/templates
 
 yarn openapi-generator-cli generate -i openapi.json -g java -o java/lib/generated/openapi -c java/openapi-generator-config.json -t java/templates
+
+yarn openapi-generator-cli generate -i openapi.json -g kotlin -o kotlin/lib/generated/openapi -c kotlin/openapi-generator-config.json -t kotlin/templates
