aef40e1edfedda4e168851149403683342ea21ac
Tal Hacohen <hacohen.tal@gmail.com>
2021-09-18 10:09:58 +0300

    Change model package to com.svix.kotlin.models

--- kotlin/lib/src/main/kotlin/Authentication.kt
+++ kotlin/lib/src/main/kotlin/Authentication.kt
@@ -2,7 +2,7 @@ package com.svix.kotlin
 
 import com.svix.kotlin.internal.apis.AuthenticationApi
 import com.svix.kotlin.internal.infrastructure.ServerException
-import com.svix.kotlin.internal.models.DashboardAccessOut
+import com.svix.kotlin.models.DashboardAccessOut
 
 class Authentication() {
     val api = AuthenticationApi()
