711ed819e68a56fa5a6a9e1ee776151cc6092e68
Tal Hacohen <hacohen.tal@gmail.com>
2021-09-18 12:35:44 +0300

    Add builder for options list

--- kotlin/lib/src/main/kotlin/ListOptions.kt
+++ kotlin/lib/src/main/kotlin/ListOptions.kt
@@ -1,3 +1,7 @@
 package com.svix.kotlin
 
-open class ListOptions(var iterator: String = "", var limit: Int = 50)
+open class ListOptions(var iterator: String = "", var limit: Int = 50) {
+	  fun iterator(iterator : String) = apply { this.iterator = iterator}
+	
+	  fun limit(limit : Int) = apply { this.limit = limit}
+}
