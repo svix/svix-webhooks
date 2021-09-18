0968d1d7664326b2894b8563af1f4b96690ba199
Tal Hacohen <hacohen.tal@gmail.com>
2021-09-18 12:53:28 +0300

    Convert Webhook.java to kotlin and add missing exceptions

---
+++ kotlin/lib/src/main/kotlin/exceptions/WebhookVerificationException.kt
@@ -0,0 +1,8 @@
+package com.svix.kotlin.exceptions
+
+class WebhookVerificationException: Exception {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+}
