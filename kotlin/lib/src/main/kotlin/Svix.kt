35e1a7a81a8f9d2c1d27ae09ea77541e1fa26dc6
Frank Chiarulli Jr <frank@svix.com>
2021-09-22 11:56:59 -0400

    update svix class

--- kotlin/lib/src/main/kotlin/Svix.kt
+++ kotlin/lib/src/main/kotlin/Svix.kt
@@ -1,9 +1,11 @@
 package com.svix.kotlin
 
+import com.svix.kotlin.internal.infrastructure.ApiClient
+
 class Svix(token: String, options: SvixOptions = SvixOptions()) {
-    companion object {
-        val VERSION = "0.29.0"
-    }
+//    companion object {
+//        val VERSION = "0.29.0"
+//    }
 
     private val application: Application
     private val authentication: Authentication
@@ -13,6 +15,10 @@ class Svix(token: String, options: SvixOptions = SvixOptions()) {
     private val messageAttempt: MessageAttempt
 
     init {
+        // FIXME the way ApiClient is generated makes it global...
+        // this means that a project can't have multiple Svix objects
+        ApiClient.accessToken = token
+
         application = Application(options)
         authentication = Authentication(options)
         endpoint = Endpoint(options)
