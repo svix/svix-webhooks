61c38a8098bc144d1b4183ea954843e34ed8a8e2
Frank Chiarulli Jr <frank@svix.com>
2021-09-21 20:22:58 -0400

    Bump version & update changelog

--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -25,4 +25,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "0.28.0"
+__version__ = "0.29.0"
--- ruby/src/svix/version.rb
+++ ruby/src/svix/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Svix
-  VERSION = "0.28.0"
+  VERSION = "0.29.0"
 end
