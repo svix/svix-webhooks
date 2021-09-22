3a2b91cf8b0fb32bba83f2574ebc719086b7febb
Frank Chiarulli Jr <frank@svix.com>
2021-09-22 15:00:30 -0400

    simplify webhook verification exception

--- kotlin/lib/src/main/kotlin/exceptions/WebhookVerificationException.kt
+++ kotlin/lib/src/main/kotlin/exceptions/WebhookVerificationException.kt
@@ -1,8 +1,3 @@
 package com.svix.kotlin.exceptions
 
-class WebhookVerificationException : Exception {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(cause: Throwable) : super(cause)
-}
+class WebhookVerificationException(message: String) : Exception(message)
