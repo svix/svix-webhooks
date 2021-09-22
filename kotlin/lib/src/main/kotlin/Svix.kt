1635e6473a8fb0e59bb6cc04c09717e007e342a5
Frank Chiarulli Jr <frank@svix.com>
2021-09-22 16:51:36 -0400

    format svix.kt

--- kotlin/lib/src/main/kotlin/Svix.kt
+++ kotlin/lib/src/main/kotlin/Svix.kt
@@ -1,13 +1,10 @@
 package com.svix.kotlin
 
-import com.svix.kotlin.internal.infrastructure.ApiClient
-
 class Svix(token: String, options: SvixOptions = SvixOptions()) {
-
     val application = Application(token, options)
     val authentication = Authentication(token, options)
     val endpoint = Endpoint(token, options)
     val eventType = EventType(token, options)
-    val message  = Message(token, options)
+    val message = Message(token, options)
     val messageAttempt = MessageAttempt(token, options)
 }
