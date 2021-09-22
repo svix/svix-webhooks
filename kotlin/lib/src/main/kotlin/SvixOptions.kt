9c01ae68041eb495e83748a5f999455cb1b91309
Frank Chiarulli Jr <frank@svix.com>
2021-09-22 17:00:55 -0400

    make version private
    reset version

--- kotlin/lib/src/main/kotlin/SvixOptions.kt
+++ kotlin/lib/src/main/kotlin/SvixOptions.kt
@@ -1,13 +1,13 @@
 package com.svix.kotlin
 
 data class SvixOptions(val debugUrl: String = DEFAULT_URL) {
-    val VERSION = "0.29.0"
+    private val version = "0.29.0"
 
     companion object {
         const val DEFAULT_URL = "https://api.svix.com"
     }
 
     internal fun getUA(): String {
-        return "svix-libs/$VERSION/kotlin"
+        return "svix-libs/$version/kotlin"
     }
 }
