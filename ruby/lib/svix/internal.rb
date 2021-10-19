5cd5ef0a3f9986a7b1e63823aed4e23bfe876917
Frank Chiarulli Jr <frank@svix.com>
2021-10-19 01:37:02 -0400

    expose api error

--- ruby/lib/svix/internal.rb
+++ ruby/lib/svix/internal.rb
@@ -2,7 +2,6 @@
 
 module Svix
     private_constant :ApiClient
-    private_constant :ApiError
     private_constant :Configuration
     private_constant :ApplicationApi
     private_constant :AuthenticationApi
