f298bd8da8df42f32d785631d4c9af8319010061
Frank <frank@svix.com>
2021-11-17 15:54:24 -0500

    Bump version & update changelog (#169)

--- go/internal/version/version.go
+++ go/internal/version/version.go
@@ -1,3 +1,3 @@
 package version
 
-const Version = "0.36.0"
+const Version = "0.37.0"
--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -25,4 +25,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "0.36.0"
+__version__ = "0.37.0"
