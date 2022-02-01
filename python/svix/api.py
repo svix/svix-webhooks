0a641cce98a432ec5a963e83009609298da227ab
stephanie federman <stephanie@svix.com>
2022-01-31 17:48:47 -0800

    Python: Add get-or-create for application

--- python/svix/api.py
+++ python/svix/api.py
@@ -138,6 +138,12 @@ class Application(ApiBase[ApplicationApi]):
         with self._api() as api:
             return api.get_application_api_v1_app_app_id_get(app_id=app_id, _check_return_type=False)
 
+    def get_or_create(self, application_in: ApplicationIn, get_if_exists: bool) -> ApplicationOut:
+        with self._api() as api:
+            return api.create_application_api_v1_app_post(
+                application_in=application_in, get_if_exists=get_if_exists, _check_return_type=False
+            )
+
     def update(self, app_id: str, application_in: ApplicationIn) -> ApplicationOut:
         with self._api() as api:
             return api.update_application_api_v1_app_app_id_put(
