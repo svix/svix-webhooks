svix-frank/svix-libs

add import export to kotlin

kotlin/lib/src/main/kotlin/Svix.kt
@@ -7,4 +7,5 @@ class Svix(token: String, options: SvixOptions = SvixOptions()) {
     val eventType = EventType(token, options)
     val message = Message(token, options)
     val messageAttempt = MessageAttempt(token, options)
+    val organization = Organization(token, options)
 }

svix/svix-webhooks

kotlin/lib/src/main/kotlin/Svix.kt
@@ -8,4 +8,5 @@ class Svix(token: String, options: SvixOptions = SvixOptions()) {
     val integration = Integration(token, options)
     val message = Message(token, options)
     val messageAttempt = MessageAttempt(token, options)
+    val environment = Environment(token, options)
 }