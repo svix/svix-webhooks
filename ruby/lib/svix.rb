svix-frank/svix-libs

add import export to ruby

ruby/lib/svix.rb
@@ -12,6 +12,8 @@ require "svix/api/event_type_api"
 require "svix/api/health_api"
 require "svix/api/message_api"
 require "svix/api/message_attempt_api"
+require "svix/api/organization_api"
+
 
 # Models
 require "svix/models/application_in"
@@ -61,6 +63,9 @@ require "svix/models/message_status"
 require "svix/models/recover_in"
 require "svix/models/validation_error"
 require "svix/models/webhook_types"
+require "svix/models/export_organization_out"
+require "svix/models/import_organization_in"
+
 
 # Core
 require "svix/api_client"
@@ -73,6 +78,8 @@ require "svix/errors"
 require "svix/event_type_api"
 require "svix/message_api"
 require "svix/message_attempt_api"
+require "svix/organization_api"
+
 require "svix/svix"
 require "svix/util"
 require "svix/version"

ruby/lib/svix/svix.rb
@@ -18,6 +18,7 @@ module Svix
         attr_accessor :event_type
         attr_accessor :message
         attr_accessor :message_attempt
+        attr_accessor :organization
 
         def initialize(auth_token, options = SvixOptions.new)
             uri = URI(options.server_url)
@@ -38,6 +39,7 @@ module Svix
             @event_type = EventTypeAPI.new(api_client)
             @message = MessageAPI.new(api_client)
             @message_attempt = MessageAttemptAPI.new(api_client)
+            @organization = OrganizationAPI.new(api_client)
         end
     end
 end

svix/svix-webhooks

ruby/lib/svix.rb
@@ -14,6 +14,8 @@ require "svix/api/health_api"
 require "svix/api/integration_api"
 require "svix/api/message_api"
 require "svix/api/message_attempt_api"
+require "svix/api/environment_api"
+
 
 # Models
 require "svix/models/application_in"
@@ -66,6 +68,9 @@ require "svix/models/message_status"
 require "svix/models/recover_in"
 require "svix/models/validation_error"
 require "svix/models/webhook_types"
+require "svix/models/export_environment_out"
+require "svix/models/import_environment_in"
+
 
 # Core
 require "svix/api_client"
@@ -79,6 +84,8 @@ require "svix/event_type_api"
 require "svix/integration_api"
 require "svix/message_api"
 require "svix/message_attempt_api"
+require "svix/environment_api"
+
 require "svix/svix"
 require "svix/util"
 require "svix/version"

ruby/lib/svix/svix.rb
@@ -19,6 +19,7 @@ module Svix
         attr_accessor :integration
         attr_accessor :message
         attr_accessor :message_attempt
+        attr_accessor :environment
 
         def initialize(auth_token, options = SvixOptions.new)
             uri = URI(options.server_url)
@@ -40,6 +41,7 @@ module Svix
             @integration = IntegrationAPI.new(api_client)
             @message = MessageAPI.new(api_client)
             @message_attempt = MessageAttemptAPI.new(api_client)
+            @environment = EnvironmentAPI.new(api_client)
         end
     end
 end