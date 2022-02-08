ca46531b33487570f19b00286c86a5436c8ab660
Stephanie Federman <97116432+svix-stephanie@users.noreply.github.com>
2022-02-08 09:13:11 -0800

    Python: Add support for idempotency_key (#230)

--- python/svix/api.py
+++ python/svix/api.py
@@ -63,6 +63,14 @@ class ListOptions:
 
 
 @dataclass
+class PostOptions:
+    idempotency_key: t.Optional[str] = None
+
+    def to_dict(self) -> t.Dict[str, t.Any]:
+        return {k: v for k, v in asdict(self).items() if v is not None}
+
+
+@dataclass
 class MessageListOptions(ListOptions):
     event_types: t.Optional[t.List[str]] = None
     before: t.Optional[datetime] = None
@@ -127,15 +135,15 @@ class ApiBase(t.Generic[ApiClass]):
 class Authentication(ApiBase[AuthenticationApi]):
     _ApiClass = AuthenticationApi
 
-    def dashboard_access(self, app_id: str) -> DashboardAccessOut:
+    def dashboard_access(self, app_id: str, options: PostOptions = PostOptions()) -> DashboardAccessOut:
         with self._api() as api:
             return api.get_dashboard_access_api_v1_auth_dashboard_access_app_id_post(
-                app_id=app_id, _check_return_type=False
+                app_id=app_id, **options.to_dict(), _check_return_type=False
             )
 
-    def logout(self) -> None:
+    def logout(self, options: PostOptions = PostOptions()) -> None:
         with self._api() as api:
-            return api.logout_api_v1_auth_logout_post(_check_return_type=False)
+            return api.logout_api_v1_auth_logout_post(**options.to_dict(), _check_return_type=False)
 
 
 class Application(ApiBase[ApplicationApi]):
@@ -145,18 +153,20 @@ class Application(ApiBase[ApplicationApi]):
         with self._api() as api:
             return api.list_applications_api_v1_app_get(**options.to_dict(), _check_return_type=False)
 
-    def create(self, application_in: ApplicationIn) -> ApplicationOut:
+    def create(self, application_in: ApplicationIn, options: PostOptions = PostOptions()) -> ApplicationOut:
         with self._api() as api:
-            return api.create_application_api_v1_app_post(application_in=application_in, _check_return_type=False)
+            return api.create_application_api_v1_app_post(
+                application_in=application_in, **options.to_dict(), _check_return_type=False
+            )
 
     def get(self, app_id: str) -> ApplicationOut:
         with self._api() as api:
             return api.get_application_api_v1_app_app_id_get(app_id=app_id, _check_return_type=False)
 
-    def get_or_create(self, application_in: ApplicationIn) -> ApplicationOut:
+    def get_or_create(self, application_in: ApplicationIn, options: PostOptions = PostOptions()) -> ApplicationOut:
         with self._api() as api:
             return api.create_application_api_v1_app_post(
-                application_in=application_in, get_if_exists=True, _check_return_type=False
+                application_in=application_in, get_if_exists=True, **options.to_dict(), _check_return_type=False
             )
 
     def update(self, app_id: str, application_in: ApplicationIn) -> ApplicationOut:
@@ -179,10 +189,10 @@ class Endpoint(ApiBase[EndpointApi]):
                 app_id=app_id, **options.to_dict(), _check_return_type=False
             )
 
-    def create(self, app_id: str, endpoint_in: EndpointIn) -> EndpointOut:
+    def create(self, app_id: str, endpoint_in: EndpointIn, options: PostOptions = PostOptions()) -> EndpointOut:
         with self._api() as api:
             return api.create_endpoint_api_v1_app_app_id_endpoint_post(
-                app_id, endpoint_in=endpoint_in, _check_return_type=False
+                app_id, endpoint_in=endpoint_in, **options.to_dict(), _check_return_type=False
             )
 
     def get(self, app_id: str, endpoint_id: str) -> EndpointOut:
@@ -209,21 +219,31 @@ class Endpoint(ApiBase[EndpointApi]):
                 app_id=app_id, endpoint_id=endpoint_id, _check_return_type=False
             )
 
-    def rotate_secret(self, app_id: str, endpoint_id: str, endpoint_secret_rotate_in: EndpointSecretRotateIn) -> None:
+    def rotate_secret(
+        self,
+        app_id: str,
+        endpoint_id: str,
+        endpoint_secret_rotate_in: EndpointSecretRotateIn,
+        options: PostOptions = PostOptions(),
+    ) -> None:
         with self._api() as api:
             return api.rotate_endpoint_secret_api_v1_app_app_id_endpoint_endpoint_id_secret_rotate_post(
                 app_id=app_id,
                 endpoint_id=endpoint_id,
                 endpoint_secret_rotate_in=endpoint_secret_rotate_in,
+                **options.to_dict(),
                 _check_return_type=False,
             )
 
-    def recover(self, app_id: str, endpoint_id: str, recover_in: RecoverIn) -> None:
+    def recover(
+        self, app_id: str, endpoint_id: str, recover_in: RecoverIn, options: PostOptions = PostOptions()
+    ) -> None:
         with self._api() as api:
             api.recover_failed_webhooks_api_v1_app_app_id_endpoint_endpoint_id_recover_post(
                 app_id=app_id,
                 endpoint_id=endpoint_id,
                 recover_in=recover_in,
+                **options.to_dict(),
                 _check_return_type=False,
             )
 
@@ -261,9 +281,11 @@ class EventType(ApiBase[EventTypeApi]):
         with self._api() as api:
             return api.list_event_types_api_v1_event_type_get(**options.to_dict(), _check_return_type=False)
 
-    def create(self, event_type_in: EventTypeIn) -> EventTypeOut:
+    def create(self, event_type_in: EventTypeIn, options: PostOptions = PostOptions()) -> EventTypeOut:
         with self._api() as api:
-            return api.create_event_type_api_v1_event_type_post(event_type_in=event_type_in, _check_return_type=False)
+            return api.create_event_type_api_v1_event_type_post(
+                event_type_in=event_type_in, **options.to_dict(), _check_return_type=False
+            )
 
     def get(self, event_type_name: str) -> EventTypeOut:
         with self._api() as api:
@@ -295,10 +317,10 @@ class Integration(ApiBase[IntegrationApi]):
                 app_id=app_id, **options.to_dict(), _check_return_type=False
             )
 
-    def create(self, app_id: str, integ_in: IntegrationIn) -> IntegrationOut:
+    def create(self, app_id: str, integ_in: IntegrationIn, options: PostOptions = PostOptions()) -> IntegrationOut:
         with self._api() as api:
             return api.create_integration_api_v1_app_app_id_integration_post(
-                app_id, integration_in=integ_in, _check_return_type=False
+                app_id, integration_in=integ_in, **options.to_dict(), _check_return_type=False
             )
 
     def get(self, app_id: str, integ_id: str) -> IntegrationOut:
@@ -325,12 +347,10 @@ class Integration(ApiBase[IntegrationApi]):
                 app_id=app_id, integ_id=integ_id, _check_return_type=False
             )
 
-    def rotate_key(self, app_id: str, integ_id: str) -> IntegrationKeyOut:
+    def rotate_key(self, app_id: str, integ_id: str, options: PostOptions = PostOptions()) -> IntegrationKeyOut:
         with self._api() as api:
             return api.rotate_integration_key_api_v1_app_app_id_integration_integ_id_key_rotate_post(
-                app_id=app_id,
-                integ_id=integ_id,
-                _check_return_type=False,
+                app_id=app_id, integ_id=integ_id, **options.to_dict(), _check_return_type=False
             )
 
 
@@ -343,10 +363,10 @@ class Message(ApiBase[MessageApi]):
                 app_id=app_id, **options.to_dict(), _check_return_type=False
             )
 
-    def create(self, app_id: str, message_in: MessageIn) -> MessageOut:
+    def create(self, app_id: str, message_in: MessageIn, options: PostOptions = PostOptions()) -> MessageOut:
         with self._api() as api:
             return api.create_message_api_v1_app_app_id_msg_post(
-                app_id=app_id, message_in=message_in, _check_return_type=False
+                app_id=app_id, message_in=message_in, **options.to_dict(), _check_return_type=False
             )
 
     def get(self, app_id: str, msg_id: str) -> MessageOut:
@@ -373,10 +393,10 @@ class MessageAttempt(ApiBase[MessageAttemptApi]):
                 app_id=app_id, msg_id=msg_id, attempt_id=attempt_id, _check_return_type=False
             )
 
-    def resend(self, app_id: str, msg_id: str, endpoint_id: str) -> None:
+    def resend(self, app_id: str, msg_id: str, endpoint_id: str, options: PostOptions = PostOptions()) -> None:
         with self._api() as api:
             return api.resend_webhook_api_v1_app_app_id_msg_msg_id_endpoint_endpoint_id_resend_post(
-                app_id=app_id, msg_id=msg_id, endpoint_id=endpoint_id, _check_return_type=False
+                app_id=app_id, msg_id=msg_id, endpoint_id=endpoint_id, **options.to_dict(), _check_return_type=False
             )
 
     def list_attempted_messages(
