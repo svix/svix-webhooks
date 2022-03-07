be1f654b9664a9351477e0b1df8dd71cdb31a92b
Daniel Lyne <daniel@svix.com>
2022-03-07 12:36:37 -0500

    Reorganize create_message_app

--- server/svix-server/src/core/cache/create_message_app.rs
+++ server/svix-server/src/core/cache/create_message_app.rs
@@ -27,47 +27,6 @@ pub struct CreateMessageApp {
     pub deleted: bool,
 }
 
-impl TryFrom<endpoint::Model> for CreateMessageEndpoint {
-    type Error = Error;
-
-    fn try_from(m: endpoint::Model) -> Result<CreateMessageEndpoint> {
-        Ok(CreateMessageEndpoint {
-            id: m.id,
-            url: m.url,
-            key: m.key,
-            old_signing_keys: m.old_keys,
-            event_types_ids: m.event_types_ids,
-            channels: m.channels,
-            rate_limit: m
-                .rate_limit
-                .map(|v| v.try_into())
-                .transpose()
-                .map_err(|_| Error::Validation("Endpoint rate limit out of bounds".to_owned()))?,
-            first_failure_at: m.first_failure_at,
-            headers: m.headers,
-            disabled: m.disabled,
-            deleted: m.deleted,
-        })
-    }
-}
-
-/// The information for each individual endpoint cached with the creation of a message.
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct CreateMessageEndpoint {
-    pub id: EndpointId,
-    pub url: String,
-    pub key: EndpointSecret,
-    pub old_signing_keys: Option<ExpiringSigningKeys>,
-    pub event_types_ids: Option<EventTypeNameSet>,
-    pub channels: Option<EventChannelSet>,
-    pub rate_limit: Option<u16>,
-    // Same type as the `DateTimeWithTimeZone from SeaORM used in the endpoint model
-    pub first_failure_at: Option<DateTime<FixedOffset>>,
-    pub headers: Option<EndpointHeaders>,
-    pub disabled: bool,
-    pub deleted: bool,
-}
-
 impl CreateMessageApp {
     /// Fetch all requisite information for creating a [`CreateMessageApp`] from the PostgreSQL
     /// database
@@ -95,6 +54,47 @@ impl CreateMessageApp {
                 })?,
             endpoints,
             deleted: app.deleted,
+        })
+    }
+}
+
+/// The information for each individual endpoint cached with the creation of a message.
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct CreateMessageEndpoint {
+    pub id: EndpointId,
+    pub url: String,
+    pub key: EndpointSecret,
+    pub old_signing_keys: Option<ExpiringSigningKeys>,
+    pub event_types_ids: Option<EventTypeNameSet>,
+    pub channels: Option<EventChannelSet>,
+    pub rate_limit: Option<u16>,
+    // Same type as the `DateTimeWithTimeZone from SeaORM used in the endpoint model
+    pub first_failure_at: Option<DateTime<FixedOffset>>,
+    pub headers: Option<EndpointHeaders>,
+    pub disabled: bool,
+    pub deleted: bool,
+}
+
+impl TryFrom<endpoint::Model> for CreateMessageEndpoint {
+    type Error = Error;
+
+    fn try_from(m: endpoint::Model) -> Result<CreateMessageEndpoint> {
+        Ok(CreateMessageEndpoint {
+            id: m.id,
+            url: m.url,
+            key: m.key,
+            old_signing_keys: m.old_keys,
+            event_types_ids: m.event_types_ids,
+            channels: m.channels,
+            rate_limit: m
+                .rate_limit
+                .map(|v| v.try_into())
+                .transpose()
+                .map_err(|_| Error::Validation("Endpoint rate limit out of bounds".to_owned()))?,
+            first_failure_at: m.first_failure_at,
+            headers: m.headers,
+            disabled: m.disabled,
+            deleted: m.deleted,
         })
     }
 }
