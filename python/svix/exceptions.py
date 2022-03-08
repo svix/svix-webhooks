9562294df7a7cd8e5bb1d3655b7017dd540feda6
stephanie federman <stephanie@svix.com>
2022-03-08 12:09:05 -0800

    Fixes related to comments and exceptions

--- python/svix/exceptions.py
+++ python/svix/exceptions.py
@@ -1,10 +1,8 @@
 from .internal.openapi_client.models.http_error import HttpError
 from .internal.openapi_client.models.http_validation_error import HTTPValidationError
-from .internal.openapi_client.models.validation_error import ValidationError
 from .webhooks import WebhookVerificationError
 
 __all__ = [
-    "ValidationError",
     "HTTPValidationError",
     "HttpError",
     "WebhookVerificationError",
