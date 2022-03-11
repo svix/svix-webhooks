6b82badce8c645580e3730bac4e35835f5a64a9f
Joshua Allen <joshua@over-one.studio>
2022-03-10 22:32:39 -0500

    Created placeholder for Event Type resource

---
+++ csharp/Svix/Abstractions/IEventType.cs
@@ -0,0 +1,25 @@
+﻿namespace Svix.Abstractions
+{
+    public interface IEventType
+    {
+        void Archive();
+        
+        void ArchiveAsync();
+
+        void Create();
+        
+        void CreateAsync();
+
+        void Get();
+        
+        void GetAsync();
+
+        void List();
+        
+        void ListAsync();
+
+        void Update();
+        
+        void UpdateAsync();
+    }
+}
---
+++ csharp/Svix/EventType.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Svix.Abstractions;
+using Svix.Api;
+
+namespace Svix
+{
+    public sealed class EventType : SvixResourceBase, IEventType
+    {
+        private readonly IEventTypeApi _eventTypeApi;
+        
+        public EventType(ISvixClient svixClient, IEventTypeApi eventTypeApi) 
+            : base(svixClient)
+        {
+            _eventTypeApi = eventTypeApi ?? throw new ArgumentNullException(nameof(eventTypeApi));
+        }
+
+        public void Archive()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void ArchiveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void List()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void UpdateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
