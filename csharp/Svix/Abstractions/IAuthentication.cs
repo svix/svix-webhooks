b758696eabb95db240b77b058454b8a55bde0d9c
Joshua Allen <joshua@over-one.studio>
2022-03-10 22:32:10 -0500

    Created placeholder for Authentication resource

---
+++ csharp/Svix/Abstractions/IAuthentication.cs
@@ -0,0 +1,13 @@
+﻿namespace Svix.Abstractions
+{
+    public interface IAuthentication
+    {
+        void GetDashboardAccess();
+        
+        void GetDashboardAccessAsync();
+
+        void Logout();
+        
+        void LogoutAsync();
+    }
+}
---
+++ csharp/Svix/Authentication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Svix.Abstractions;
+using Svix.Api;
+
+namespace Svix
+{
+    public sealed class Authentication : SvixResourceBase, IAuthentication
+    {
+        private readonly IAuthenticationApi _authenticationApi;
+        
+        public Authentication(ISvixClient svixClient, IAuthenticationApi authenticationApi) 
+            : base(svixClient)
+        {
+            _authenticationApi = authenticationApi ?? throw new ArgumentNullException(nameof(authenticationApi));
+        }
+
+        public void GetDashboardAccess()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetDashboardAccessAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LogoutAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
