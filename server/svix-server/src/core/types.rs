937c54e4d4fffb6c86458418986e9c1c7976cdff
Daniel Lyne <100604337+svix-daniel@users.noreply.github.com>
2022-03-28 14:36:09 -0400

    Simplify Validate impl for IdOrUid types (#347)

--- server/svix-server/src/core/types.rs
+++ server/svix-server/src/core/types.rs
@@ -345,13 +345,7 @@ macro_rules! create_all_id_types {
 
         impl Validate for $name_id_or_uid {
             fn validate(&self) -> Result<(), validator::ValidationErrors> {
-                if self.0.starts_with($key_prefix) {
-                    let as_id: $name_id = self.clone().into();
-                    as_id.validate()
-                } else {
-                    let as_uid: $name_uid = self.clone().into();
-                    as_uid.validate()
-                }
+                validate_limited_str(&self.0)
             }
         }
     };
