c1e256b70d59b6c6c6873d351ba0f0bc6658d5af
Dylan Taylor <dylan@svix.com>
2022-04-22 15:33:07 +0000

    replicate unit tests for MemoryCache

--- server/svix-server/src/core/cache/memory.rs
+++ server/svix-server/src/core/cache/memory.rs
@@ -117,3 +117,111 @@ impl CacheBehavior for MemoryCache {
 fn check_is_expired(vw: &ValueWrapper) -> bool {
     vw.timer.elapsed().as_millis() <= vw.ttl.as_millis()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{super::kv_def, *};
+    use serde::{Deserialize, Serialize};
+
+    // Test structures
+
+    #[derive(Deserialize, Serialize, Debug, PartialEq)]
+    struct TestValA(usize);
+    kv_def!(TestKeyA, TestValA);
+    impl TestKeyA {
+        fn new(id: String) -> TestKeyA {
+            TestKeyA(format!("SVIX_TEST_KEY_A_{}", id))
+        }
+    }
+
+    #[derive(Deserialize, Serialize, Debug, PartialEq)]
+    struct TestValB(String);
+    kv_def!(TestKeyB, TestValB);
+    impl TestKeyB {
+        fn new(id: String) -> TestKeyB {
+            TestKeyB(format!("SVIX_TEST_KEY_B_{}", id))
+        }
+    }
+
+    #[tokio::test]
+    async fn test_cache_crud_no_ttl() {
+        let cache = new();
+
+        let (first_key, first_val_a, first_val_b) =
+            (TestKeyA::new("1".to_owned()), TestValA(1), TestValA(2));
+        let (second_key, second_val_a, second_val_b) = (
+            TestKeyB::new("1".to_owned()),
+            TestValB("1".to_owned()),
+            TestValB("2".to_owned()),
+        );
+
+        // Create
+        assert!(cache
+            .set(&first_key, &first_val_a, Duration::from_secs(30),)
+            .await
+            .is_ok());
+        assert!(cache
+            .set(&second_key, &second_val_a, Duration::from_secs(30),)
+            .await
+            .is_ok());
+
+        // Read
+        assert_eq!(cache.get(&first_key).await.unwrap(), Some(first_val_a));
+        assert_eq!(cache.get(&second_key).await.unwrap(), Some(second_val_a));
+
+        // Update (overwrite)
+        assert!(cache
+            .set(&first_key, &first_val_b, Duration::from_secs(30),)
+            .await
+            .is_ok());
+        assert!(cache
+            .set(&second_key, &second_val_b, Duration::from_secs(30),)
+            .await
+            .is_ok());
+
+        // Confirm update
+        assert_eq!(cache.get(&first_key).await.unwrap(), Some(first_val_b));
+        assert_eq!(cache.get(&second_key).await.unwrap(), Some(second_val_b));
+
+        // Delete
+        assert!(cache.delete(&first_key).await.is_ok());
+        assert!(cache.delete(&second_key).await.is_ok());
+
+        // Confirm deletion
+        assert_eq!(cache.get::<TestValA>(&first_key).await.unwrap(), None);
+        assert_eq!(cache.get::<TestValB>(&second_key).await.unwrap(), None);
+    }
+
+    #[tokio::test]
+    async fn test_cache_ttl() {
+        let cache = new();
+        let key = TestKeyA::new("key".to_owned());
+
+        assert!(cache
+            .set(&key, &TestValA(1), Duration::from_secs(1),)
+            .await
+            .is_ok());
+        tokio::time::sleep(std::time::Duration::from_millis(1200)).await;
+        assert_eq!(cache.get::<TestValA>(&key).await.unwrap(), None);
+    }
+
+    #[tokio::test]
+    async fn test_cache_nx_status() {
+        let cache = new();
+        let key = TestKeyA::new("nx_status_test_key".to_owned());
+
+        assert!(cache
+            .set_if_not_exists(&key, &TestValA(1), Duration::from_secs(30),)
+            .await
+            .unwrap());
+        assert_eq!(cache.get(&key).await.unwrap(), Some(TestValA(1)));
+
+        assert!(!cache
+            .set_if_not_exists(&key, &TestValA(2), Duration::from_secs(30),)
+            .await
+            .unwrap());
+        assert_eq!(cache.get(&key).await.unwrap(), Some(TestValA(1)));
+
+        assert!(cache.delete(&key).await.is_ok());
+    }
+}
