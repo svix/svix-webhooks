bfe5bb9e561fd6f99a2d414ab76c9eed6ebb9053
Dylan Taylor <dylan@svix.com>
2022-05-02 19:12:03 +0000

    partial index filters non-null payloads from query

--- server/svix-server/migrations/20220428190104_message_expiration.down.sql
+++ server/svix-server/migrations/20220428190104_message_expiration.down.sql
@@ -1,2 +1,5 @@
+BEGIN;
 ALTER TABLE message DROP COLUMN expiration;
 ALTER TABLE message ALTER COLUMN payload SET NOT NULL;
+DROP INDEX message_payload_idx;
+COMMIT;
--- server/svix-server/migrations/20220428190104_message_expiration.up.sql
+++ server/svix-server/migrations/20220428190104_message_expiration.up.sql
@@ -1,2 +1,5 @@
+BEGIN;
 ALTER TABLE message ADD COLUMN expiration timestamp with time zone;
 ALTER TABLE message ALTER COLUMN payload DROP NOT NULL;
+CREATE INDEX ON message (payload) WHERE payload IS NOT NULL;
+COMMIT;
