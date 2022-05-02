a40984d715ea0d6ba7cfaa6d0f537d21a2fa1b46
Dylan Taylor <dylan@svix.com>
2022-05-02 23:19:16 +0000

    add logic to gracefully migrate existing messages

--- server/svix-server/src/expired_message_cleaner.rs
+++ server/svix-server/src/expired_message_cleaner.rs
@@ -3,16 +3,27 @@
 
 use crate::db::models::message;
 use crate::error::Result;
-use chrono::Utc;
+use chrono::{Duration, Utc};
 use sea_orm::{
     entity::prelude::*, sea_query::Expr, DatabaseConnection, DbErr, EntityTrait, UpdateResult,
 };
-use tokio::time::{sleep, Duration};
+use tokio::time::sleep;
 
 /// Nullifies the payload column for expired messages
 async fn clean_expired_messages(
     pool: &DatabaseConnection,
 ) -> std::result::Result<UpdateResult, DbErr> {
+    // this logic exists to gracefully migrate existing message attempt responses
+    message::Entity::update_many()
+        .col_expr(
+            message::Column::Expiration,
+            Expr::value(Utc::now() + Duration::days(2)),
+        )
+        .filter(message::Column::Expiration.is_null())
+        .exec(pool)
+        .await
+        .expect("Update of existing messageattempt NULL timestamps failed");
+
     message::Entity::update_many()
         .col_expr(message::Column::Payload, Expr::value(Value::Json(None)))
         .filter(message::Column::Expiration.lte(Utc::now()))
@@ -24,7 +35,7 @@ async fn clean_expired_messages(
 /// Runs every 5 minutes
 pub async fn expired_message_cleaner_loop(pool: &DatabaseConnection) -> Result<()> {
     loop {
-        sleep(Duration::from_secs(5 * 60)).await;
+        sleep(std::time::Duration::from_secs(5)).await;
         let pool = pool.clone();
         if let Err(err) = clean_expired_messages(&pool).await {
             tracing::error!("{}", err)
