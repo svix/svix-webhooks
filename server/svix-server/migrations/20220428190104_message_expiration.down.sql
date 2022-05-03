aaf702ad7de183edf709b348b50ed1cffe414951
Dylan Taylor <dylan@svix.com>
2022-05-03 21:12:19 +0000

    migrations are already transaction blocks

--- server/svix-server/migrations/20220428190104_message_expiration.down.sql
+++ server/svix-server/migrations/20220428190104_message_expiration.down.sql
@@ -1,5 +1,3 @@
-BEGIN;
 ALTER TABLE message DROP COLUMN expiration;
 ALTER TABLE message ALTER COLUMN payload SET NOT NULL;
 DROP INDEX message_payload_idx;
-COMMIT;
--- server/svix-server/migrations/20220428190104_message_expiration.up.sql
+++ server/svix-server/migrations/20220428190104_message_expiration.up.sql
@@ -1,5 +1,3 @@
-BEGIN;
 ALTER TABLE message ADD COLUMN expiration timestamp with time zone;
 ALTER TABLE message ALTER COLUMN payload DROP NOT NULL;
 CREATE INDEX ON message (payload) WHERE payload IS NOT NULL;
-COMMIT;
