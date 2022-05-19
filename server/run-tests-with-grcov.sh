8e4de041009ccdf39de9294d3c014646f91360b8
Dylan Taylor <dylan@svix.com>
2022-05-19 17:44:22 +0000

    feedback

--- server/run-tests-with-grcov.sh
+++ server/run-tests-with-grcov.sh
@@ -1,20 +1,20 @@
 #!/bin/sh -e
 
-# install deps
-cargo install grcov
-rustup component add llvm-tools-preview
-
-# set env vars
+# cleanly run the following commands in their own session
+bash -c "
+# tell Rust to run with coverage instrumentation
 RUSTFLAGS="-Cinstrument-coverage"
+# give grcov a profile name template for output files
 LLVM_PROFILE_FILE="svix-webhooks-%p-%m.profraw"
+# put the compiler in nightly mode
 RUSTC_BOOTSTRAP=1
 
 # run tests
 ./run-tests.sh
-
-# back to stable
-RUSTC_BOOTSTRAP=0
+" || true
 
 # generate and open report output
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
-open ./target/debug/coverage/index.html
+
+LOCATION="coverage HTML file at $(pwd)/target/debug/coverage/index.html"
+echo "$LOCATION"
