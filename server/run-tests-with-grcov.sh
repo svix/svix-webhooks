78e089fc3a2a67ad8c01967b130710a174a40463
Dylan Taylor <dylan@svix.com>
2022-05-19 16:55:40 +0000

    gitignore and results streamlining

--- server/run-tests-with-grcov.sh
+++ server/run-tests-with-grcov.sh
@@ -1,8 +1,20 @@
+#!/bin/sh -e
+
+# install deps
+cargo install grcov
 rustup component add llvm-tools-preview
 
+# set env vars
 RUSTFLAGS="-Cinstrument-coverage"
 LLVM_PROFILE_FILE="svix-webhooks-%p-%m.profraw"
+RUSTC_BOOTSTRAP=1
 
+# run tests
 ./run-tests.sh
 
+# back to stable
+RUSTC_BOOTSTRAP=0
+
+# generate and open report output
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
+open ./target/debug/coverage/index.html
