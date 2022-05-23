8ebe00f3a7d709a59bc10d2c1062ee6761eeb670
Dylan Taylor <dylan@svix.com>
2022-05-23 12:28:21 +0000

    support --run-migrations option on server startup

--- server/svix-server/src/main.rs
+++ server/svix-server/src/main.rs
@@ -13,6 +13,7 @@ use svix_server::{cfg, db, run};
 
 const CRATE_NAME: &str = env!("CARGO_CRATE_NAME");
 
+use crate::cfg::Configuration;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
@@ -20,6 +21,9 @@ use clap::{Parser, Subcommand};
 struct Args {
     #[clap(subcommand)]
     command: Option<Commands>,
+    /// Run database migrations before starting
+    #[clap(long)]
+    run_migrations: bool,
 }
 
 #[derive(Subcommand)]
@@ -30,7 +34,7 @@ enum Commands {
         #[clap(subcommand)]
         command: JwtCommands,
     },
-
+    /// Run database migrations and exit
     #[clap()]
     Migrate,
 }
@@ -62,9 +66,17 @@ async fn main() {
 
     tracing_subscriber::fmt::init();
 
-    if let Some(Commands::Migrate) = &args.command {
-        let _ = db::init_db_and_run_migrations(&cfg).await;
+    async fn run_migrations(cfg: &Configuration) {
+        db::init_db_and_run_migrations(cfg).await;
         println!("Migrations run");
+    }
+
+    if args.run_migrations {
+        run_migrations(&cfg).await;
+    }
+
+    if let Some(Commands::Migrate) = &args.command {
+        run_migrations(&cfg).await;
         exit(0);
     } else if let Some(Commands::Jwt {
         command: JwtCommands::Generate,
