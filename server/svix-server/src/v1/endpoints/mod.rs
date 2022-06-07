b96f9bd430ae5b3ef94cc3652a7af2de340fe204
Daniel Lyne <daniel@svix.com>
2022-06-07 12:37:02 -0400

    Run rustfmt

--- server/svix-server/src/v1/endpoints/mod.rs
+++ server/svix-server/src/v1/endpoints/mod.rs
@@ -1,9 +1,9 @@
 // SPDX-FileCopyrightText: © 2022 Svix Authors
 // SPDX-License-Identifier: MIT
 
-pub mod auth;
 pub mod application;
 pub mod attempt;
+pub mod auth;
 pub mod endpoint;
 pub mod event_type;
 pub mod health;
