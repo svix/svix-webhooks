6c8ba715d9331aa89e9d3316e5cd168a77b621c9
Tom Hacohen <tasn@users.noreply.github.com>
2022-06-07 18:30:19 +0300

    C#: expose the missing getters for making it useful (#486)
    We were not exposing many needed getters, including the ones for getting
    the message creator (and many others).
    
    Without them being exposed, you can't actually interact with the API.
    This fixes it.
    
    Fixes #485

--- csharp/Svix/Abstractions/ISvixClient.cs
+++ csharp/Svix/Abstractions/ISvixClient.cs
@@ -8,6 +8,18 @@ namespace Svix.Abstractions
     {
         public IApplication Application { get; }
 
+        public IAuthentication Authentication { get; }
+
+        public IEndpoint Endpoint { get; }
+
+        public IEventType EventType { get; }
+
+        public IIntegration Integration { get; }
+
+        public IMessage Message { get; }
+
+        public IMessageAttempt MessageAttempt { get; }
+
         public IHealth Health { get; }
 
         public ILogger Logger { get; }
