2d86726a45660fd74da2735d2cf0873e98e9441e
Daniel Lyne <daniel@svix.com>
2022-06-20 10:48:59 -0400

    Add simple wait-for functionality

--- server/svix-server/src/main.rs
+++ server/svix-server/src/main.rs
@@ -5,8 +5,11 @@
 #![forbid(unsafe_code)]
 
 use dotenv::dotenv;
-use std::process::exit;
+use trust_dns_resolver::TokioHandle;
+use std::net::IpAddr;
+use std::{process::exit, time::Duration};
 use svix_server::core::types::OrganizationId;
+use trust_dns_resolver::{config::{ResolverConfig, ResolverOpts}, TokioAsyncResolver};
 use validator::Validate;
 
 use svix_server::core::security::{default_org_id, generate_org_token};
@@ -22,9 +25,14 @@ use clap::{Parser, Subcommand};
 struct Args {
     #[clap(subcommand)]
     command: Option<Commands>,
+
     /// Run database migrations before starting
     #[clap(long, value_parser)]
     run_migrations: bool,
+
+    /// The time to wait for a successful connection to the database before timing out in seconds.
+    #[clap(long, value_parser)]
+    wait_for: Option<u64>,
 }
 
 #[derive(Subcommand)]
@@ -114,6 +122,81 @@ async fn main() {
         }
         None => {}
     };
+
+    if let Some(wait_for_seconds) = args.wait_for {
+        let pg = &cfg.db_dsn;
+
+        let pg = url::Url::parse(pg).expect("Invalid PostgreSQL DNS");
+
+        let pg_host = pg.host().expect("Expected PostgreSQL host");
+        let pg_port = pg.port().unwrap_or(5432);
+
+        let pg_host: IpAddr = match pg_host {
+            url::Host::Domain(s) => {
+                let resolver = TokioAsyncResolver::new(
+                    ResolverConfig::default(), ResolverOpts::default(), TokioHandle)
+                    .expect("Could not set up DNS resolution");
+
+                let resp = resolver.lookup_ip(s).await.expect("DNS error");
+                resp.iter()
+                    .next()
+                    .expect("Expected at least one IP address")
+            }
+            url::Host::Ipv4(ipv4_addr) => ipv4_addr.into(),
+            url::Host::Ipv6(ipv6_addr) => ipv6_addr.into(),
+        };
+
+        let sleep = tokio::time::sleep(Duration::from_secs(wait_for_seconds));
+        tokio::pin!(sleep);
+        loop {
+            tokio::select! {
+                _ = tokio::net::TcpStream::connect((pg_host, pg_port)) => {
+                    break;
+                },
+                _ = &mut sleep => {
+                    panic!("Waiting for databases timed out");
+                }
+            }
+        }
+
+        let redis = cfg.redis_dsn.as_deref();
+
+        if let Some(redis) = redis {
+            let redis = url::Url::parse(redis).expect("Invalid Redis DSN");
+
+            let redis_host = redis.host().expect("Exepected Redis host");
+            let redis_port = redis.port().unwrap_or(6379);
+
+            let redis_host: IpAddr = match redis_host {
+                url::Host::Domain(s) => {
+                let resolver = TokioAsyncResolver::new(
+                    ResolverConfig::default(), ResolverOpts::default(), TokioHandle)
+                    .expect("Could not set up DNS resolution");
+
+                let resp = resolver.lookup_ip(s).await.expect("DNS error");
+                resp.iter()
+                    .next()
+                    .expect("Expected at least one IP address")
+
+                }
+                url::Host::Ipv4(ipv4_addr) => ipv4_addr.into(),
+                url::Host::Ipv6(ipv6_addr) => ipv6_addr.into(),
+            };
+
+            let sleep = tokio::time::sleep(Duration::from_secs(wait_for_seconds));
+            tokio::pin!(sleep);
+            loop {
+                tokio::select! {
+                    _ = tokio::net::TcpStream::connect((redis_host, redis_port)) => {
+                        break;
+                    },
+                    _ = &mut sleep => {
+                        panic!("Waiting for databases timed out");
+                    }
+                }
+            }
+        }
+    }
 
     run(cfg, None).await;
 }
