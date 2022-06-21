d05e2e4294722254d683608fe4825f0941062178
Daniel Lyne <daniel@svix.com>
2022-06-21 09:24:38 -0400

    Get system working with Docker

--- server/svix-server/src/wait_for.rs
+++ server/svix-server/src/wait_for.rs
@@ -1,9 +1,4 @@
-use std::{net::IpAddr, time::Duration};
-
-use trust_dns_resolver::{
-    config::{ResolverConfig, ResolverOpts},
-    TokioAsyncResolver, TokioHandle,
-};
+use std::time::Duration;
 
 pub async fn wait_for_dsn(
     dsn: &str,
@@ -18,18 +13,12 @@ pub async fn wait_for_dsn(
         .unwrap_or_else(|| panic!("Expected {} host", dependency_name));
     let port = dsn.port().unwrap_or(default_port);
 
-    let host_ip: IpAddr = match host {
-        url::Host::Domain(s) => resolve_host(s).await,
-        url::Host::Ipv4(ipv4_addr) => ipv4_addr.into(),
-        url::Host::Ipv6(ipv6_addr) => ipv6_addr.into(),
-    };
-
     let sleep = tokio::time::sleep(Duration::from_secs(wait_for_seconds));
     tokio::pin!(sleep);
 
     loop {
         tokio::select! {
-            res = tokio::net::TcpStream::connect((host_ip, port)) => {
+            res = tokio::net::TcpStream::connect((host.to_string(), port)) => {
                 if res.is_err() {
                     continue;
                 } else {
@@ -38,22 +27,8 @@ pub async fn wait_for_dsn(
             }
 
             _ = &mut sleep => {
-                panic!("Waiting for host={} ({}) port={} timed out", host, host_ip, port);
+                panic!("Waiting for host={} port={} timed out", host, port);
             }
         }
     }
-}
-
-async fn resolve_host(host: &str) -> IpAddr {
-    let resolver = TokioAsyncResolver::new(
-        ResolverConfig::default(),
-        ResolverOpts::default(),
-        TokioHandle,
-    )
-    .expect("Could not set up DNS resolution");
-
-    let resp = resolver.lookup_ip(host).await.expect("DNS error");
-    resp.iter()
-        .next()
-        .expect("Expected at least one IP address")
 }
