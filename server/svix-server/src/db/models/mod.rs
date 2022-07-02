810786fe060f60a4f5a53b53f32cbf1868b4f1bf
00imvj00 <vijaybambhaniya007@gmail.com>
2022-07-02 20:08:50 +0530

    feat(model): pg table model added into db module

--- server/svix-server/src/db/models/mod.rs
+++ server/svix-server/src/db/models/mod.rs
@@ -7,3 +7,4 @@ pub mod eventtype;
 pub mod message;
 pub mod messageattempt;
 pub mod messagedestination;
+pub mod pg_queue;
---
+++ server/svix-server/src/db/models/pg_queue.rs
@@ -0,0 +1,19 @@
+use chrono::Utc;
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, DeriveEntityModel, PartialEq)]
+#[sea_orm(table_name = "queue")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i64,
+    pub queue_name: String,
+    #[sea_orm(column_type = "JsonBinary", nullable)]
+    pub payload: Option<Json>,
+    pub when_run: i64,
+    pub created_at: chrono::DateTime<Utc>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
