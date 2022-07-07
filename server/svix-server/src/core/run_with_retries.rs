d47283e5e1dfdb4d51ad4505afa3892815a6baba
Daniel Lyne <daniel@svix.com>
2022-07-07 09:44:59 -0400

    Fix review concerns

--- server/svix-server/src/core/run_with_retries.rs
+++ server/svix-server/src/core/run_with_retries.rs
@@ -1,26 +1,31 @@
 use std::{future::Future, time::Duration};
 
-pub async fn run_with_retries<T, E, F: Future<Output = Result<T, E>>, FN: FnMut() -> F>(
+pub async fn run_with_retries<
+    T,
+    E: std::error::Error,
+    F: Future<Output = Result<T, E>>,
+    FN: FnMut() -> F,
+>(
     mut fun: FN,
     should_retry: impl Fn(&E) -> bool,
     retry_schedule: &[Duration],
 ) -> Result<T, E> {
-    let mut num_attempts = 0;
-
-    loop {
+    for duration in retry_schedule {
         let fun_ret = fun().await;
         match fun_ret {
-            Ok(_) => break fun_ret,
+            Ok(_) => return fun_ret,
             Err(ref e) => {
-                if num_attempts == retry_schedule.len() {
-                    break fun_ret;
-                } else if should_retry(e) {
-                    tokio::time::sleep(retry_schedule[num_attempts]).await;
-                    num_attempts += 1;
+                if should_retry(e) {
+                    tracing::warn!("Retrying after error {}", e);
+                    tokio::time::sleep(*duration).await;
                 } else {
-                    break fun_ret;
+                    tracing::warn!("Unrecoverable error {}", e);
+                    return fun_ret;
                 }
             }
         }
     }
+
+    tracing::warn!("Exhausted retry schedule");
+    fun().await
 }
