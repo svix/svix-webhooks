d57f80429f2a42b07d128b88c473022b3c055f73
Dylan Taylor <dylan@svix.com>
2022-07-21 21:32:15 +0000

    imperative go impl

--- go/svix.go
+++ go/svix.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/url"
 	"time"
+	"strings"
 
 	"github.com/svix/svix-webhooks/go/internal/openapi"
 	"github.com/svix/svix-webhooks/go/internal/version"
@@ -45,6 +46,21 @@ func New(token string, options *SvixOptions) *Svix {
 	conf.Scheme = "https"
 	conf.Host = "api.svix.com"
 	conf.HTTPClient = defaultHTTPClient
+
+	var tokenParts = strings.Split(s, ".")
+	if len(tokenParts) == 2 {
+		var region = tokenParts[1]
+		if region == "us" {
+			conf.Host = "api.us.svix.com"
+		}
+		if region == "eu" {
+			conf.Host = "api.eu.svix.com"
+		}
+		if region == "in" {
+			conf.Host = "api.in.svix.com"
+		}
+	}
+
 	if options != nil {
 		conf.Debug = options.Debug
 		if options.ServerUrl != nil {
