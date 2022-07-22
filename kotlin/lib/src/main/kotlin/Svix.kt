a429d8a3f1d3b18fa729d3e0f4d441ad3420ccde
Dylan Taylor <dylan@svix.com>
2022-07-22 16:06:36 -0400

    Kotlin (thank you Tom!)

--- kotlin/lib/src/main/kotlin/Svix.kt
+++ kotlin/lib/src/main/kotlin/Svix.kt
@@ -2,21 +2,17 @@ package com.svix.kotlin
 
 class Svix(token: String, options: SvixOptions = SvixOptions()) {
 
-    val tokenParts = token.split(".")
-    if (options.serverUrl == null) {
-        if (tokenParts.size == 2) {
-          val region = tokenParts[1]
-          if (region == "us") {
-            options.serverUrl = "https://api.us.svix.com"
-          }
-          if (region == "eu") {
-            options.serverUrl = "https://api.eu.svix.com"
-          }
-          if (region == "in") {
-            options.serverUrl = "https://api.in.svix.com"
-          }
-        } else {
-            options.serverUrl = "https://api.svix.com"
+    init {
+        val tokenParts = token.split(".")
+        if (options.wantedServerUrl == null) {
+            val region = tokenParts.last()
+            if (region == "us") {
+                options.serverUrl = "https://api.us.svix.com"
+            } else if (region == "eu") {
+                options.serverUrl = "https://api.eu.svix.com"
+            } else if (region == "in") {
+                options.serverUrl = "https://api.in.svix.com"
+            }
         }
     }
 
--- kotlin/lib/src/main/kotlin/SvixOptions.kt
+++ kotlin/lib/src/main/kotlin/SvixOptions.kt
@@ -1,7 +1,13 @@
 package com.svix.kotlin
 
-data class SvixOptions(val serverUrl: String?) {
+data class SvixOptions(internal var wantedServerUrl: String? = null) {
     private val version = "0.63.1"
+
+    var serverUrl: String
+        get() = this.wantedServerUrl ?: DEFAULT_URL
+        set(value) {
+            this.wantedServerUrl = value
+        }
 
     companion object {
         const val DEFAULT_URL = "https://api.svix.com"
