56a3b71b2876be14e777ac3ab60f0e81b3bba7c1
Andreas Eriksson <andreas.eriksson@voyado.com>
2022-08-17 10:35:14 +0200

    Make the logger optional in constructor

---
+++ csharp/Svix.Tests/SvixClientTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Svix.Models;
+using Xunit;
+
+namespace Svix.Tests
+{
+    public class SvixClientTests
+    {
+        [Fact]
+        public void Constructor_WhenCalled_DoesNotNeedLogger()
+        {
+            var sut = new SvixClient("", new SvixOptions("http://some.url"));
+
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void Constructor_WhenCalled_AcceptsLogger()
+        {
+            var sut = new SvixClient("", new SvixOptions("http://some.url"), new NullLogger<SvixClient>());
+
+            Assert.NotNull(sut);
+        }
+    }
+}
