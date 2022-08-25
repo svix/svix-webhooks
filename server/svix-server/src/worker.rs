0db6216abec0534fc883daa84d9fc6c065c6a2ce
Daniel Lyne <100604337+svix-daniel@users.noreply.github.com>
2022-08-25 10:22:58 -0400

    Ensure tracing spans are always recorded (#637)
    While we only officially support info, debug, and trace log levels via the
    configuration, these changes ensure that request and worker spans are entered
    even if the log level is set to warn or error via an environment variable
    override.
    
    Note that, by an error level span, that means that the span is created and
    entered for every log level, not that it only appears for errors.

--- server/svix-server/src/worker.rs
+++ server/svix-server/src/worker.rs
@@ -234,6 +234,7 @@ struct DispatchExtraIds<'a> {
         endp_id = msg_task.endpoint_id.0.as_str(),
         msg_id = msg_task.msg_id.0.as_str()
     )
+    level = "error"
 )]
 async fn dispatch(
     WorkerContext {
@@ -531,7 +532,7 @@ fn bytes_to_string(bytes: bytes::Bytes) -> String {
 }
 
 /// Manages preparation and execution of a QueueTask type
-#[tracing::instrument(skip_all, fields(task_id = worker_context.task_id))]
+#[tracing::instrument(skip_all, fields(task_id = worker_context.task_id), level = "error")]
 async fn process_task(worker_context: WorkerContext<'_>, queue_task: Arc<QueueTask>) -> Result<()> {
     let WorkerContext { db, cache, .. }: WorkerContext<'_> = worker_context;
 
