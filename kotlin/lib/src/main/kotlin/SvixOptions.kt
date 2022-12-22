0a5ab3755a9d79c830dafca32edcb843825751e9
Tom Hacohen <tasn@users.noreply.github.com>
2022-12-22 10:28:31 -0500

    Bump version and update changelog. (#737)

--- kotlin/lib/src/main/kotlin/SvixOptions.kt
+++ kotlin/lib/src/main/kotlin/SvixOptions.kt
@@ -6,7 +6,7 @@ data class SvixOptions(
     val numRetries: Int? = null
 ) {
 
-    private val version = "0.71.0"
+    private val version = "0.72.0"
 
     var serverUrl: String
         get() = this.wantedServerUrl ?: DEFAULT_URL
--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -37,4 +37,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "0.71.0"
+__version__ = "0.72.0"
