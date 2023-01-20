5c70b42423e27ba0d7460ba9f2866918c66eea0c
svix-andor <120067937+svix-andor@users.noreply.github.com>
2023-01-20 14:50:17 +0100

    Add new endpoint for app portal access (#773)
    ## Motivation
    2 reasons for this endpoint:
    * Better naming than current "dashboard access"
    * This one requires a body, even if it's just an empty JSON object with
    no keys. This way we can add body parameters later without breaking
    existing clients.
    
    ## Solution
    Functionally the same as the existing `/dashboard-access/` endpoint but
    we require a request body on this endpoint.

--- server/svix-server/src/v1/endpoints/auth.rs
+++ server/svix-server/src/v1/endpoints/auth.rs
@@ -18,19 +18,25 @@ pub struct DashboardAccessOut {
 }
 
 #[derive(Deserialize, Serialize, Validate, JsonSchema)]
-pub struct DashboardAccessIn {
+pub struct AppPortalAccessIn {
     /// The set of feature flags the created token will have access to.
     #[serde(default, skip_serializing_if = "FeatureFlagSet::is_empty")]
     pub feature_flags: FeatureFlagSet,
 }
 
-async fn dashboard_access(
+pub type AppPortalAccessOut = DashboardAccessOut;
+
+async fn app_portal_access(
     State(AppState { cfg, .. }): State<AppState>,
     permissions::OrganizationWithApplication { app }: permissions::OrganizationWithApplication,
-    data: Option<ValidatedJson<DashboardAccessIn>>,
-) -> Result<Json<DashboardAccessOut>> {
-    let feature_flags = data.map(|data| data.0.feature_flags).unwrap_or_default();
-    let token = generate_app_token(&cfg.jwt_secret, app.org_id, app.id.clone(), feature_flags)?;
+    ValidatedJson(data): ValidatedJson<AppPortalAccessIn>,
+) -> Result<Json<AppPortalAccessOut>> {
+    let token = generate_app_token(
+        &cfg.jwt_secret,
+        app.org_id,
+        app.id.clone(),
+        data.feature_flags,
+    )?;
 
     let login_key = serde_json::to_vec(&serde_json::json!({
         "appId": app.id,
@@ -47,6 +53,20 @@ async fn dashboard_access(
     Ok(Json(DashboardAccessOut { url, token }))
 }
 
+async fn dashboard_access(
+    state: State<AppState>,
+    permissions: permissions::OrganizationWithApplication,
+) -> Result<Json<DashboardAccessOut>> {
+    app_portal_access(
+        state,
+        permissions,
+        ValidatedJson(AppPortalAccessIn {
+            feature_flags: FeatureFlagSet::default(),
+        }),
+    )
+    .await
+}
+
 pub fn router() -> ApiRouter<AppState> {
     ApiRouter::new()
         .api_route_with(
@@ -57,6 +77,11 @@ pub fn router() -> ApiRouter<AppState> {
         .api_route_with(
             "/auth/logout/",
             post(api_not_implemented),
+            openapi_tag("Authentication"),
+        )
+        .api_route_with(
+            "/auth/app-portal-access/:app_id/",
+            post(app_portal_access),
             openapi_tag("Authentication"),
         )
 }
