e1417a34568dafcfac555bc36bff74aa489677b7
Tom Hacohen <tasn@users.noreply.github.com>
2023-02-09 19:43:26 +0100

    Bump version and update changelog. (#832)

--- go/internal/version/version.go
+++ go/internal/version/version.go
@@ -1,3 +1,3 @@
 package version
 
-const Version = "0.77.0"
+const Version = "0.78.0"
--- kotlin/lib/src/main/kotlin/SvixOptions.kt
+++ kotlin/lib/src/main/kotlin/SvixOptions.kt
@@ -6,7 +6,7 @@ data class SvixOptions(
     val numRetries: Int? = null
 ) {
 
-    private val version = "0.77.0"
+    private val version = "0.78.0"
 
     var serverUrl: String
         get() = this.wantedServerUrl ?: DEFAULT_URL
