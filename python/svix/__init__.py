d24866b5c0b726ae26882cc92830eb51d0b1f4a9
Tom Hacohen <tom@stosb.com>
2023-03-21 21:44:35 +0200

    Bump version and update changelog.

--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -37,4 +37,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "0.81.0"
+__version__ = "0.82.0"
--- ruby/lib/svix/version.rb
+++ ruby/lib/svix/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Svix
-  VERSION = "0.81.0"
+  VERSION = "0.82.0"
 end
