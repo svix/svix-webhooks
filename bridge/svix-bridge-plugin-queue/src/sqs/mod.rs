fdc5b1ad35d4dc3be6d551362c4ebe7f7fe4aaa9
svix-onelson <123012825+svix-onelson@users.noreply.github.com>
2023-05-24 09:59:07 -0700

    polish bridge config (#925)
    Alright y'all. After a bunch of conversations about the overall config
    and mental model to apply to the project (from the user perspective),
    this PR ended up _huge_. It'll be a tough review. I'm sorry :sob:
    
    This is a pretty large shift.
    
    Here's a breakdown of the broad strokes:
    
    - The `Plugin` trait was removed from `-types`.
    - New traits added: `SenderInput` and `ReceiverOutput` to represent the
      non-svix portions of the dataflow.
    - Removed the `-plugin-webhook-receiver` plugin crate.
    - moved the http server from the `-plugin-webhook-receiver` crate into
    `svix-bridge`.
      - "queue forwarders" into the `-plugin-queue-consumer` crate.
    - Renamed the `-plugin-queue-consumer` crate to just `-plugin-queue` (it
    now provides both `SenderInput` and `ReceiverOutput` implementation for
    the various queue backends.
    - all crates etc have been renamed: `s/svix-webhook-bridge/svix-bridge/`
    
    
    What else, what else, what else...
    
    The example config has been split into 2: one showing `senders` and
    another for `receivers`.
    The readme has been gutted (but probably needs to be fleshed out a bit
    more... again). It felt silly to effectively duplicate the example
    configs in code fence blocks a 2nd time.
    
    To sweeten the deal, new test coverage has been added for select
    portions of the config parsing as well as for the HTTP receiver stuff.
    
    Both the sender and receiver example configs are now checked in the test
    suite to make sure they at least parse without failure. These tests
    should be expanded to inspect the actual data, but this diff is so
    big... :sob:
    
    We didn't have any tests for the HTTP handler execution before this, so
    that's very nice to have.

---
+++ bridge/svix-bridge-plugin-queue/src/sqs/mod.rs
@@ -0,0 +1,99 @@
+use crate::error::Error;
+use crate::{run_inner, Consumer, ConsumerWrapper};
+use generic_queue::{
+    sqs::{SqsConfig, SqsQueueBackend},
+    TaskQueueBackend,
+};
+use serde::Deserialize;
+use svix_bridge_types::{
+    async_trait, svix::api::Svix, JsObject, SenderInput, SenderOutputOpts, TransformerTx,
+};
+
+#[derive(Debug, Default, Deserialize)]
+pub struct SqsInputOpts {
+    pub queue_dsn: String,
+    #[serde(default)]
+    pub override_endpoint: bool,
+}
+
+pub struct SqsConsumerPlugin {
+    name: String,
+    input_options: SqsInputOpts,
+    svix_client: Svix,
+    transformer_tx: Option<TransformerTx>,
+    transformation: Option<String>,
+}
+
+impl SqsConsumerPlugin {
+    pub fn new(
+        name: String,
+        input: SqsInputOpts,
+        transformation: Option<String>,
+        output: SenderOutputOpts,
+    ) -> Self {
+        Self {
+            name,
+            input_options: input,
+            svix_client: match output {
+                SenderOutputOpts::Svix(output) => {
+                    Svix::new(output.token, output.options.map(Into::into))
+                }
+            },
+            transformer_tx: None,
+            transformation,
+        }
+    }
+}
+
+#[async_trait]
+impl Consumer for SqsConsumerPlugin {
+    fn source(&self) -> &str {
+        &self.input_options.queue_dsn
+    }
+
+    fn system(&self) -> &str {
+        "sqs"
+    }
+
+    fn transformer_tx(&self) -> &Option<TransformerTx> {
+        &self.transformer_tx
+    }
+
+    fn transformation(&self) -> &Option<String> {
+        &self.transformation
+    }
+
+    fn svix_client(&self) -> &Svix {
+        &self.svix_client
+    }
+
+    async fn consumer(&self) -> std::io::Result<ConsumerWrapper> {
+        let consumer = <SqsQueueBackend as TaskQueueBackend<JsObject>>::consuming_half(SqsConfig {
+            queue_dsn: self.input_options.queue_dsn.clone(),
+            override_endpoint: self.input_options.override_endpoint,
+        })
+        .await
+        .map_err(Error::from)?;
+        Ok(ConsumerWrapper::SQS(consumer))
+    }
+}
+
+#[async_trait]
+impl SenderInput for SqsConsumerPlugin {
+    fn name(&self) -> &str {
+        &self.name
+    }
+    fn set_transformer(&mut self, tx: Option<TransformerTx>) {
+        self.transformer_tx = tx;
+    }
+    async fn run(&self) -> std::io::Result<()> {
+        run_inner(self).await
+    }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct SqsOutputOpts {
+    pub queue_dsn: String,
+    #[serde(default)]
+    pub override_endpoint: bool,
+}
