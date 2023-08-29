1f91ca978528a105c7bff861f0694558b2b9c1d3
Tom Hacohen <tasn@users.noreply.github.com>
2023-08-29 11:32:32 -0400

    Bump version and update changelog. (#1058)

--- kotlin/lib/src/main/kotlin/SvixOptions.kt
+++ kotlin/lib/src/main/kotlin/SvixOptions.kt
@@ -6,7 +6,7 @@ data class SvixOptions(
     val numRetries: Int? = null
 ) {
 
-    private val version = "1.8.1"
+    private val version = "1.9.0"
 
     var serverUrl: String
         get() = this.wantedServerUrl ?: DEFAULT_URL
