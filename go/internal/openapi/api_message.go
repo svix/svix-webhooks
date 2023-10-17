a8e624b8005cc5f59c2b71d6fb779b7cf9541bb4
svix-lucho <lucho@svix.com>
2023-10-17 15:56:44 -0300

    Update OpenAPI spec and update libs

--- go/internal/openapi/api_message.go
+++ go/internal/openapi/api_message.go
@@ -420,6 +420,16 @@ func (a *MessageApiService) V1MessageCreateExecute(r ApiV1MessageCreateRequest) 
 			newErr.model = v
 			return localVarReturnValue, localVarHTTPResponse, newErr
 		}
+		if localVarHTTPResponse.StatusCode == 413 {
+			var v HttpErrorOut
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
 		if localVarHTTPResponse.StatusCode == 422 {
 			var v HTTPValidationError
 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
