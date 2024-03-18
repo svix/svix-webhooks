5731406d896d2186d55e80c61931ff69933d52d5
Jonas Platte <jplatte@svix.com>
2024-03-18 12:45:27 +0100

    bridge: Use cargo-nextest for running tests when available

--- bridge/run-tests.sh
+++ bridge/run-tests.sh
@@ -1,8 +1,20 @@
-#!/bin/sh -e
+#!/bin/bash
+
+if [[ -z "$TEST_COMMAND" ]]; then
+    if [[ -z "$CARGO_HOME" ]]; then
+        CARGO_HOME="$HOME/.cargo"
+    fi
+
+    if command -v cargo-nextest || [[ -e "$CARGO_HOME/bin/cargo-nextest" ]]; then
+        TEST_COMMAND="cargo nextest run"
+    else
+        TEST_COMMAND="cargo test"
+    fi
+fi
 
 AWS_DEFAULT_REGION="elasticmq" \
 AWS_ACCESS_KEY_ID="x" \
 AWS_SECRET_ACCESS_KEY="x" \
 PUBSUB_EMULATOR_HOST=localhost:8085 \
 PUBSUB_PROJECT_ID=local-project \
-cargo test --all-features -- "$@"
+${TEST_COMMAND} --all-features -- "$@"
