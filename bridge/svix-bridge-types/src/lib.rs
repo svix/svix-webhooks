b55b3eac08ff0a9ed481971749cbf0542531f46c
Jonas Platte <jplatte@svix.com>
2024-04-02 17:07:38 +0200

    bridge: Separate first line of docs for SenderInput from rest

--- bridge/svix-bridge-types/src/lib.rs
+++ bridge/svix-bridge-types/src/lib.rs
@@ -128,6 +128,7 @@ impl TransformerJob {
 }
 
 /// Effectively a black box to the supervisor.
+///
 /// Plugins should run until they are done, and likely they should not be "done" until the program
 /// exits.
 #[async_trait]
