2a8e93d2028a84a9399e82f398508d8dbf0c6a43
James Lucas <jaymell@users.noreply.github.com>
2024-05-08 14:59:13 -0500

    Prepare 1.24.0 release (#1315)
    Bump lib versions / update ChangeLog

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -77,7 +77,7 @@ import * as base64 from "@stablelib/base64";
 import * as sha256 from "fast-sha256";
 
 const WEBHOOK_TOLERANCE_IN_SECONDS = 5 * 60; // 5 minutes
-const VERSION = "1.23.0";
+const VERSION = "1.24.0";
 
 class UserAgentMiddleware implements Middleware {
   public pre(context: RequestContext): Promise<RequestContext> {
@@ -397,7 +397,7 @@ class Endpoint {
   }
 
   /**
-   * @deprecated Since version 1.23.0. Use `headersUpdate` instead.
+   * @deprecated Since version 1.24.0. Use `headersUpdate` instead.
    */
   public updateHeaders(
     appId: string,
@@ -424,7 +424,7 @@ class Endpoint {
   }
 
   /**
-   * @deprecated Since version 1.23.0. Use `headersPatch` instead.
+   * @deprecated Since version 1.24.0. Use `headersPatch` instead.
    */
   public patchHeaders(
     appId: string,
