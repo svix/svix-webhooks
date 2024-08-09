178d24d8a16eb96ae2c17dceac3e58f1f2205132
svix-lucho <133019767+svix-lucho@users.noreply.github.com>
2024-08-09 12:43:53 -0300

    Bump version to 1.29.0 and update changelog (#1398)
    ## Version 1.29.0
    * Bridge: Rebuild RabbitMQ producer on error
    * Server: Add support for SOCKS5 proxies
    * Libs/C#: add GetAppPortalAccess* methods to IAuthentication interface
    * Libs/Go: expose `with_content` for `Message.List`
    * Libs/Go: expose `with_msg` param on `MessageAttempt.ListByEndpoint`
    * Libs/Go: expose `order` in `Integtation.List` and `EventType.List`
    * Libs/Go: return response body for recover/replay
    * Libs/Go: expose `tag` for `MessageAttempt` list endpoints
    * Libs/Javascript: Publish src to npm to make it available for source
    maps
    * Libs/Python: ensure datetimes have a timezone

--- go/internal/version/version.go
+++ go/internal/version/version.go
@@ -1,3 +1,3 @@
 package version
 
-const Version = "1.28.0"
+const Version = "1.29.0"
--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -37,4 +37,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "1.28.0"
+__version__ = "1.29.0"
