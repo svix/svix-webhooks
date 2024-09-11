949cfa9706d7ee2b60fce51a696f02231da2f2ce
Jonas Platte <jplatte@svix.com>
2024-09-11 18:45:16 +0200

    Bump version and update changelog

--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -37,4 +37,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "1.32.0"
+__version__ = "1.33.0"
--- ruby/lib/svix/version.rb
+++ ruby/lib/svix/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Svix
-  VERSION = "1.32.0"
+  VERSION = "1.33.0"
 end
