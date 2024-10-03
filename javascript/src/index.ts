5e4e33dea498b2ca5ca7d688bf525fbfb42bd49c
svix-lucho <lucho@svix.com>
2024-10-03 16:53:18 -0300

    Bump version to 1.38.0 and update changelog

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -77,7 +77,7 @@ import * as base64 from "@stablelib/base64";
 import * as sha256 from "fast-sha256";
 
 const WEBHOOK_TOLERANCE_IN_SECONDS = 5 * 60; // 5 minutes
-const VERSION = "1.37.0";
+const VERSION = "1.38.0";
 
 class UserAgentMiddleware implements Middleware {
   public pre(context: RequestContext): Promise<RequestContext> {
