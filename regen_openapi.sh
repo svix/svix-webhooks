113a754279008a9fce56c24b64b3aeaa6920a691
Owen Nelson <onelson@svix.com>
2024-10-16 10:53:52 -0700

    Libs(JS): reintroduce template overrides

--- regen_openapi.sh
+++ regen_openapi.sh
@@ -6,7 +6,7 @@ if [ -n "$1" ]; then
     curl "$1" | python -m json.tool > openapi.json
 fi
 
-yarn openapi-generator-cli generate -i openapi.json -g typescript -o javascript/src/openapi -c javascript/openapi-generator-config.json --type-mappings=set=Array # -t javascript/templates
+yarn openapi-generator-cli generate -i openapi.json -g typescript -o javascript/src/openapi -c javascript/openapi-generator-config.json --type-mappings=set=Array -t javascript/templates
 
 # Cleanup previous codegen, allowing us to spot removals.
 # If the removals are expected, stage them eg. `git add -u`, then commit them.
