15453dfdef474290970fcd81a9626159674af0b8
Owen Nelson <onelson@svix.com>
2024-10-16 16:18:35 -0700

    Libs(JS): add "kitchen sink" test to show serialization works
    I've been adding a test like this to each lib as I go while updating the
    generator version. This should ensure the serialization is doing the
    right thing with regards to nulls, and decoding response bodies even
    while we're removing some of the patches we put in place to fix these
    things.
    
    Set `SVIX_TOKEN` and `SVIX_SERVER_URL` to run this against a live
    `svix-server` instance.

--- javascript/src/KitchenSink.test.ts
+++ javascript/src/KitchenSink.test.ts
@@ -1,13 +1,15 @@
-import {ApplicationIn, Svix} from ".";
+import { ApiException, HttpErrorOut, Svix } from ".";
 
-function getTestClient() : Svix | null {
-    const token = process.env.SVIX_TOKEN;
-    const serverUrl = process.env.SVIX_SERVER_URL;
-    if (!token || !serverUrl) {
-        console.warn("Unable to construct test client: `SVIX_TOKEN` or `SVIX_SERVER_URL` unset.")
-        return null;
-    }
-    return new Svix(token, { serverUrl });
+function getTestClient(): Svix | null {
+  const token = process.env.SVIX_TOKEN;
+  const serverUrl = process.env.SVIX_SERVER_URL;
+  if (!token || !serverUrl) {
+    console.warn(
+      "Unable to construct test client: `SVIX_TOKEN` or `SVIX_SERVER_URL` unset."
+    );
+    return null;
+  }
+  return new Svix(token, { serverUrl });
 }
 const client = getTestClient();
 
@@ -15,6 +17,40 @@ const client = getTestClient();
 test = client == null ? test.skip : test;
 
 test("endpoint crud", async () => {
-  const appOut = await client?.application.create({ name: "App"});
-  expect(appOut?.id).toBe("xxx");
+  const appOut = await client!.application.create({ name: "App" });
+  try {
+    await client!.eventType.create({
+      name: "event.started",
+      description: "Something started",
+    });
+  } catch (e) {
+    // Conflicts are expected from test run to test run, but other statuses are not.
+    expect((e as ApiException<HttpErrorOut>).code).toEqual(409);
+  }
+  try {
+    await client!.eventType.create({
+      name: "event.ended",
+      description: "Something ended",
+    });
+  } catch (e) {
+    // Conflicts are expected from test run to test run, but other statuses are not.
+    expect((e as ApiException<HttpErrorOut>).code).toEqual(409);
+  }
+
+  const epOut = await client!.endpoint.create(appOut!.id!, {
+    url: "https://example.svix.com/",
+    channels: ["ch0", "ch1"],
+  });
+  expect(epOut!.channels!.sort()).toEqual(["ch0", "ch1"]);
+  expect(epOut!.filterTypes || []).toEqual([]);
+
+  const epPatched = await client!.endpoint.patch(appOut!.id!, epOut!.id!, {
+    filterTypes: ["event.started", "event.ended"],
+  });
+
+  expect(epPatched!.channels!.sort()).toEqual(["ch0", "ch1"]);
+  expect(epPatched!.filterTypes!.sort()).toEqual(["event.ended", "event.started"]);
+
+  // Should not throw an error while trying to deserialize the empty body.
+  await client!.endpoint.delete(appOut!.id!, epOut!.id!);
 });
