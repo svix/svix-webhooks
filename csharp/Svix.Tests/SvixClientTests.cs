8c6ea05d629a283a01e9af601fb0d954e23b0eb6
Owen Nelson <onelson@svix.com>
2024-10-16 21:13:28 -0700

    Revert "Libs(CSharp): add kitchen sink test"
    This reverts commit 1f05d0eb8d2af3860762aa60c44f4f42c4512f24.
    
    The test new runs fine on dotnet 8.0, but CI complains about something
    in the source using language features too new.
    
    The project currently targets dotnet 5.0, which seems to be EOL, so we
    should look at updating the target first, after we can un-revert this
    test.
    
    For now, just know the test passed for me with a tweak to the Svix.Test
    solution so it can run on 8.0. This gives me some confidence that the
    generator bump produced some okay code.

--- csharp/Svix.Tests/SvixClientTests.cs
+++ csharp/Svix.Tests/SvixClientTests.cs
@@ -1,74 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Extensions.Logging.Abstractions;
-using Svix.Client;
-using Svix.Model;
+﻿using Microsoft.Extensions.Logging.Abstractions;
 using Svix.Models;
 using Xunit;
 
-namespace Svix.Tests;
-
-public class SvixClientTests
+namespace Svix.Tests
 {
-    [Fact]
-    public void Constructor_WhenCalled_DoesNotNeedLogger()
+    public class SvixClientTests
     {
-        var sut = new SvixClient("", new SvixOptions("http://some.url"));
-
-        Assert.NotNull(sut);
-    }
-
-    [Fact]
-    public void Constructor_WhenCalled_AcceptsLogger()
-    {
-        var sut = new SvixClient("", new SvixOptions("http://some.url"), new NullLogger<SvixClient>());
-
-        Assert.NotNull(sut);
-    }
-
-    [IgnoreIfClientVarsUnset]
-    public void KitchenSink_SeemsToWorkOkay()
-    {
-        var token = Environment.GetEnvironmentVariable("SVIX_TOKEN");
-        var url = Environment.GetEnvironmentVariable("SVIX_SERVER_URL");
-        var client = new SvixClient(token, new SvixOptions(url));
-
-        var app = client.Application.Create(new ApplicationIn(name: "App"));
-        try
+        [Fact]
+        public void Constructor_WhenCalled_DoesNotNeedLogger()
         {
-            client.EventType.Create(new EventTypeIn(name: "event.started", description: "Something started"));
-        }
-        catch (ApiException e)
-        {
-            // We expect conflicts, but any other status is an error
-            Assert.Equal(409, e.ErrorCode);
+            var sut = new SvixClient("", new SvixOptions("http://some.url"));
+
+            Assert.NotNull(sut);
         }
 
-        try
+        [Fact]
+        public void Constructor_WhenCalled_AcceptsLogger()
         {
-            client.EventType.Create(new EventTypeIn(name: "event.ended", description: "Something ended"));
+            var sut = new SvixClient("", new SvixOptions("http://some.url"), new NullLogger<SvixClient>());
+
+            Assert.NotNull(sut);
         }
-        catch (ApiException e)
-        {
-            // We expect conflicts, but any other status is an error
-            Assert.Equal(409, e.ErrorCode);
-        }
-
-        var ep = client.Endpoint.Create(app.Id,
-            new EndpointIn(url: "https://example.svix.com/", channels: new List<string> { "ch0", "ch1" }));
-
-        ep.Channels.Sort();
-        Assert.Equal(new List<string> { "ch0", "ch1" }, ep.Channels);
-        Assert.Null(ep.FilterTypes);
-
-        var epPatched = client.Endpoint.Patch(app.Id, ep.Id,
-            new EndpointPatch(filterTypes: new List<string> { "event.started", "event.ended" }));
-        epPatched.Channels.Sort();
-        epPatched.FilterTypes.Sort();
-        Assert.Equal(new List<string> { "ch0", "ch1" }, epPatched.Channels);
-        Assert.Equal(new List<string> { "event.ended", "event.started" }, epPatched.FilterTypes);
-
-        // Should not throw an exception if the serialization code handles empty bodies properly
-        client.Endpoint.Delete(app.Id, ep.Id);
     }
 }
