b0af708573d3fb6cf37fdb049ac9ed84001b925e
Jonas Platte <jplatte@svix.com>
2024-11-05 16:05:13 +0100

    python: Add operational webhook endpoint API

--- python/svix/api.py
+++ python/svix/api.py
@@ -46,6 +46,15 @@ from .internal.openapi_client.api.endpoint import (
     v1_endpoint_update,
     v1_endpoint_update_headers,
 )
+from .internal.openapi_client.api.webhook_endpoint import (
+    create_operational_webhook_endpoint,
+    delete_operational_webhook_endpoint,
+    get_operational_webhook_endpoint,
+    get_operational_webhook_endpoint_secret,
+    list_operational_webhook_endpoints,
+    rotate_operational_webhook_endpoint_secret,
+    update_operational_webhook_endpoint,
+)
 from .internal.openapi_client.api.event_type import (
     v1_event_type_create,
     v1_event_type_delete,
@@ -109,6 +118,21 @@ from .internal.openapi_client.models.endpoint_secret_out import EndpointSecretOu
 from .internal.openapi_client.models.endpoint_secret_rotate_in import (
     EndpointSecretRotateIn,
 )
+from .internal.openapi_client.models.operational_webhook_endpoint_in import (
+    OperationalWebhookEndpointIn,
+)
+from .internal.openapi_client.models.operational_webhook_endpoint_out import (
+    OperationalWebhookEndpointOut,
+)
+from .internal.openapi_client.models.operational_webhook_endpoint_secret_in import (
+    OperationalWebhookEndpointSecretIn,
+)
+from .internal.openapi_client.models.operational_webhook_endpoint_secret_out import (
+    OperationalWebhookEndpointSecretOut,
+)
+from .internal.openapi_client.models.operational_webhook_endpoint_update import (
+    OperationalWebhookEndpointUpdate,
+)
 from .internal.openapi_client.models.endpoint_stats import EndpointStats
 from .internal.openapi_client.models.endpoint_transformation_in import (
     EndpointTransformationIn,
@@ -143,6 +167,9 @@ from .internal.openapi_client.models.list_response_endpoint_message_out import (
 )
 from .internal.openapi_client.models.list_response_endpoint_out import (
     ListResponseEndpointOut,
+)
+from .internal.openapi_client.models.list_response_operational_webhook_endpoint_out import (
+    ListResponseOperationalWebhookEndpointOut,
 )
 from .internal.openapi_client.models.list_response_event_type_out import (
     ListResponseEventTypeOut,
@@ -265,6 +292,11 @@ class EndpointListOptions(ListOptions):
 class EndpointStatsOptions:
     since: t.Optional[datetime] = None
     until: t.Optional[datetime] = None
+
+
+@dataclass
+class OperationalWebhookEndpointListOptions(ListOptions):
+    order: t.Optional[Ordering] = None
 
 
 @dataclass
@@ -1399,6 +1431,128 @@ class Statistics(ApiBase):
         return v1_statistics_aggregate_event_types.request_sync(client=self._client)
 
 
+class OperationalWebhookEndpointAsync(ApiBase):
+    async def list(
+        self,
+        options: OperationalWebhookEndpointListOptions = OperationalWebhookEndpointListOptions(),
+    ) -> ListResponseOperationalWebhookEndpointOut:
+        return await list_operational_webhook_endpoints.request_asyncio(
+            client=self._client,
+            **options.to_dict(),
+        )
+
+    async def create(
+        self,
+        endpoint_in: OperationalWebhookEndpointIn,
+        options: PostOptions = PostOptions(),
+    ) -> OperationalWebhookEndpointOut:
+        return await create_operational_webhook_endpoint.request_asyncio(
+            client=self._client,
+            json_body=endpoint_in,
+            **options.to_dict(),
+        )
+
+    async def get(self, endpoint_id: str) -> OperationalWebhookEndpointOut:
+        return await get_operational_webhook_endpoint.request_asyncio(
+            client=self._client, endpoint_id=endpoint_id
+        )
+
+    async def update(
+        self, endpoint_id: str, endpoint_update: OperationalWebhookEndpointUpdate
+    ) -> OperationalWebhookEndpointOut:
+        return await update_operational_webhook_endpoint.request_asyncio(
+            client=self._client,
+            endpoint_id=endpoint_id,
+            json_body=endpoint_update,
+        )
+
+    async def delete(self, endpoint_id: str) -> None:
+        return await delete_operational_webhook_endpoint.request_asyncio(
+            client=self._client,
+            endpoint_id=endpoint_id,
+        )
+
+    async def get_secret(self, endpoint_id: str) -> OperationalWebhookEndpointSecretOut:
+        return await get_operational_webhook_endpoint_secret.request_asyncio(
+            client=self._client,
+            endpoint_id=endpoint_id,
+        )
+
+    async def rotate_secret(
+        self,
+        endpoint_id: str,
+        endpoint_secret_rotate_in: OperationalWebhookEndpointSecretIn,
+        options: PostOptions = PostOptions(),
+    ) -> None:
+        return await rotate_operational_webhook_endpoint_secret.request_asyncio(
+            client=self._client,
+            endpoint_id=endpoint_id,
+            json_body=endpoint_secret_rotate_in,
+            **options.to_dict(),
+        )
+
+
+class OperationalWebhookEndpoint(ApiBase):
+    def list(
+        self,
+        options: OperationalWebhookEndpointListOptions = OperationalWebhookEndpointListOptions(),
+    ) -> ListResponseOperationalWebhookEndpointOut:
+        return list_operational_webhook_endpoints.request_sync(
+            client=self._client,
+            **options.to_dict(),
+        )
+
+    def create(
+        self,
+        endpoint_in: OperationalWebhookEndpointIn,
+        options: PostOptions = PostOptions(),
+    ) -> OperationalWebhookEndpointOut:
+        return create_operational_webhook_endpoint.request_sync(
+            client=self._client,
+            json_body=endpoint_in,
+            **options.to_dict(),
+        )
+
+    def get(self, endpoint_id: str) -> OperationalWebhookEndpointOut:
+        return get_operational_webhook_endpoint.request_sync(
+            client=self._client, endpoint_id=endpoint_id
+        )
+
+    def update(
+        self, endpoint_id: str, endpoint_update: OperationalWebhookEndpointUpdate
+    ) -> OperationalWebhookEndpointOut:
+        return update_operational_webhook_endpoint.request_sync(
+            client=self._client,
+            endpoint_id=endpoint_id,
+            json_body=endpoint_update,
+        )
+
+    def delete(self, endpoint_id: str) -> None:
+        return delete_operational_webhook_endpoint.request_sync(
+            client=self._client,
+            endpoint_id=endpoint_id,
+        )
+
+    def get_secret(self, endpoint_id: str) -> OperationalWebhookEndpointSecretOut:
+        return get_operational_webhook_endpoint_secret.request_sync(
+            client=self._client,
+            endpoint_id=endpoint_id,
+        )
+
+    def rotate_secret(
+        self,
+        endpoint_id: str,
+        endpoint_secret_rotate_in: OperationalWebhookEndpointSecretIn,
+        options: PostOptions = PostOptions(),
+    ) -> None:
+        return rotate_operational_webhook_endpoint_secret.request_sync(
+            client=self._client,
+            endpoint_id=endpoint_id,
+            json_body=endpoint_secret_rotate_in,
+            **options.to_dict(),
+        )
+
+
 class ClientBase:
     _client: AuthenticatedClient
 
@@ -1464,6 +1618,10 @@ class SvixAsync(ClientBase):
     def statistics(self) -> StatisticsAsync:
         return StatisticsAsync(self._client)
 
+    @property
+    def operational_webhook_endpoint(self) -> OperationalWebhookEndpointAsync:
+        return OperationalWebhookEndpointAsync(self._client)
+
 
 class Svix(ClientBase):
     @property
@@ -1497,6 +1655,10 @@ class Svix(ClientBase):
     @property
     def statistics(self) -> Statistics:
         return Statistics(self._client)
+
+    @property
+    def operational_webhook_endpoint(self) -> OperationalWebhookEndpointAsync:
+        return OperationalWebhookEndpointAsync(self._client)
 
 
 __all__ = [
