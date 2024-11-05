26e9086cf0f474204e23687827964be200f76ef7
Jonas Platte <jplatte@svix.com>
2024-11-05 15:33:21 +0100

    rust: Add operational webhook endpoint API

--- rust/src/api.rs
+++ rust/src/api.rs
@@ -4,8 +4,18 @@ use hyper_util::{client::legacy::Client as HyperClient, rt::TokioExecutor};
 
 use crate::{
     apis::{
-        application_api, authentication_api, background_tasks_api, endpoint_api, event_type_api,
-        integration_api, message_api, message_attempt_api, statistics_api,
+        application_api,
+        authentication_api,
+        background_tasks_api,
+        endpoint_api,
+        event_type_api,
+        integration_api,
+        message_api,
+        message_attempt_api,
+        statistics_api,
+        // unclear where 'operational_' got dropped in the codegen, but it's a private module and
+        // the types inside it use the 'Operational' prefix so it doesn't really matter
+        webhook_endpoint_api as operational_webhook_endpoint_api,
     },
     error::Result,
     Configuration,
@@ -140,6 +150,10 @@ impl Svix {
 
     pub fn message_attempt(&self) -> MessageAttempt<'_> {
         MessageAttempt::new(&self.cfg)
+    }
+
+    pub fn operational_webhook_endpoint(&self) -> OperationalWebhookEndpoint<'_> {
+        OperationalWebhookEndpoint::new(&self.cfg)
     }
 
     pub fn statistics(&self) -> Statistics<'_> {
@@ -1262,6 +1276,122 @@ impl<'a> MessageAttempt<'a> {
                 app_id,
                 msg_id,
                 attempt_id,
+            },
+        )
+        .await
+    }
+}
+
+#[derive(Default)]
+pub struct OperationalWebhookEndpointListOptions {
+    pub iterator: Option<String>,
+    pub limit: Option<i32>,
+    pub order: Option<Ordering>,
+}
+
+pub struct OperationalWebhookEndpoint<'a> {
+    cfg: &'a Configuration,
+}
+
+impl<'a> OperationalWebhookEndpoint<'a> {
+    fn new(cfg: &'a Configuration) -> Self {
+        Self { cfg }
+    }
+
+    pub async fn list(
+        &self,
+        options: Option<OperationalWebhookEndpointListOptions>,
+    ) -> Result<ListResponseOperationalWebhookEndpointOut> {
+        let OperationalWebhookEndpointListOptions {
+            iterator,
+            limit,
+            order,
+        } = options.unwrap_or_default();
+        operational_webhook_endpoint_api::list_operational_webhook_endpoints(
+            self.cfg,
+            operational_webhook_endpoint_api::ListOperationalWebhookEndpointsParams {
+                order,
+                iterator,
+                limit,
+            },
+        )
+        .await
+    }
+
+    pub async fn create(
+        &self,
+        endpoint_in: OperationalWebhookEndpointIn,
+        options: Option<PostOptions>,
+    ) -> Result<OperationalWebhookEndpointOut> {
+        let PostOptions { idempotency_key } = options.unwrap_or_default();
+        operational_webhook_endpoint_api::create_operational_webhook_endpoint(
+            self.cfg,
+            operational_webhook_endpoint_api::CreateOperationalWebhookEndpointParams {
+                operational_webhook_endpoint_in: endpoint_in,
+                idempotency_key,
+            },
+        )
+        .await
+    }
+
+    pub async fn get(&self, endpoint_id: String) -> Result<OperationalWebhookEndpointOut> {
+        operational_webhook_endpoint_api::get_operational_webhook_endpoint(
+            self.cfg,
+            operational_webhook_endpoint_api::GetOperationalWebhookEndpointParams { endpoint_id },
+        )
+        .await
+    }
+
+    pub async fn update(
+        &self,
+        endpoint_id: String,
+        endpoint_update: OperationalWebhookEndpointUpdate,
+        _: Option<PostOptions>,
+    ) -> Result<OperationalWebhookEndpointOut> {
+        operational_webhook_endpoint_api::update_operational_webhook_endpoint(
+            self.cfg,
+            operational_webhook_endpoint_api::UpdateOperationalWebhookEndpointParams {
+                endpoint_id,
+                operational_webhook_endpoint_update: endpoint_update,
+            },
+        )
+        .await
+    }
+
+    pub async fn delete(&self, endpoint_id: String) -> Result<()> {
+        operational_webhook_endpoint_api::delete_operational_webhook_endpoint(
+            self.cfg,
+            operational_webhook_endpoint_api::DeleteOperationalWebhookEndpointParams {
+                endpoint_id,
+            },
+        )
+        .await
+    }
+
+    pub async fn get_secret(
+        &self,
+        endpoint_id: String,
+    ) -> Result<OperationalWebhookEndpointSecretOut> {
+        operational_webhook_endpoint_api::get_operational_webhook_endpoint_secret(
+            self.cfg,
+            operational_webhook_endpoint_api::GetOperationalWebhookEndpointSecretParams {
+                endpoint_id,
+            },
+        )
+        .await
+    }
+
+    pub async fn rotate_secret(
+        &self,
+        endpoint_id: String,
+        endpoint_secret_rotate_in: OperationalWebhookEndpointSecretIn,
+    ) -> Result<()> {
+        operational_webhook_endpoint_api::rotate_operational_webhook_endpoint_secret(
+            self.cfg,
+            operational_webhook_endpoint_api::RotateOperationalWebhookEndpointSecretParams {
+                endpoint_id,
+                operational_webhook_endpoint_secret_in: endpoint_secret_rotate_in,
+                idempotency_key: None,
             },
         )
         .await
