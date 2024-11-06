2c9b9a4f3bab0b3ae6cb8d59fc616c92e51ed3ca
Jonas Platte <jplatte@svix.com>
2024-11-06 10:49:13 +0100

    csharp: Add operational webhook endpoint API

---
+++ csharp/Svix/Abstractions/IOperationalWebhookEndpoint.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Svix.Model;
+using Svix.Models;
+
+namespace Svix.Abstractions
+{
+    public interface IOperationalWebhookEndpoint
+    {
+        OperationalWebhookEndpointOut Create(OperationalWebhookEndpointIn endpoint,
+            string idempotencyKey = default);
+
+        Task<OperationalWebhookEndpointOut> CreateAsync(OperationalWebhookEndpointIn endpoint,
+            string idempotencyKey = default, CancellationToken cancellationToken = default);
+
+        bool Delete(string endpointId, string idempotencyKey = default);
+
+        Task<bool> DeleteAsync(string endpointId, string idempotencyKey = default,
+            CancellationToken cancellationToken = default);
+
+        OperationalWebhookEndpointOut Get(string endpointId, string idempotencyKey = default);
+
+        Task<OperationalWebhookEndpointOut> GetAsync(string endpointId, string idempotencyKey = default,
+            CancellationToken cancellationToken = default);
+
+        string GetSecret(string endpointId, string idempotencyKey = default);
+
+        Task<string> GetSecretAsync(string endpointId, string idempotencyKey = default,
+            CancellationToken cancellationToken = default);
+
+        ListResponseOperationalWebhookEndpointOut List(ListOptions options = null,
+            string idempotencyKey = default);
+
+        Task<ListResponseOperationalWebhookEndpointOut> ListAsync(ListOptions options = null,
+            string idempotencyKey = default, CancellationToken cancellationToken = default);
+
+        bool RotateSecret(string endpointId, OperationalWebhookEndpointSecretIn secret,
+            string idempotencyKey = default);
+
+        Task<bool> RotateSecretAsync(string endpointId, OperationalWebhookEndpointSecretIn secret,
+            string idempotencyKey = default, CancellationToken cancellationToken = default);
+
+        OperationalWebhookEndpointOut Update(string endpointId,
+            OperationalWebhookEndpointUpdate endpoint, string idempotencyKey = default);
+
+        Task<OperationalWebhookEndpointOut> UpdateAsync(string endpointId,
+            OperationalWebhookEndpointUpdate endpoint, string idempotencyKey = default,
+            CancellationToken cancellationToken = default);
+    }
+}
