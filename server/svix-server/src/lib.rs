8194d18093ea4aeeea076b2fe895c076a26384d2
James Lucas <jaymell@users.noreply.github.com>
2024-11-11 11:11:47 -0600

    Remove toplevel span
    This looks like a probable cause of memory-leaks, since the
    span will never complete while the application is running.

--- server/svix-server/src/lib.rs
+++ server/svix-server/src/lib.rs
@@ -87,7 +87,6 @@ async fn graceful_shutdown_handler() {
     SHUTTING_DOWN.store(true, Ordering::SeqCst)
 }
 
-#[tracing::instrument(name = "app_start", level = "trace", skip_all)]
 pub async fn run(cfg: Configuration) {
     let _metrics = setup_metrics(&cfg);
     run_with_prefix(None, cfg, None).await
