eabe2347c3b81c5f043d9ce0e04ef00843a883b6
Jonas Platte <jplatte@svix.com>
2024-11-25 18:37:59 +0100

    kotlin: Add convenient construction of rawPayload messages

--- kotlin/lib/src/main/kotlin/Message.kt
+++ kotlin/lib/src/main/kotlin/Message.kt
@@ -3,6 +3,7 @@ package com.svix.kotlin
 import com.svix.kotlin.exceptions.ApiException
 import com.svix.kotlin.internal.apis.MessageApi
 import com.svix.kotlin.models.ListResponseMessageOut
+import com.svix.kotlin.models.ApplicationIn
 import com.svix.kotlin.models.MessageIn
 import com.svix.kotlin.models.MessageOut
 
@@ -70,4 +71,44 @@ class Message internal constructor(token: String, options: SvixOptions) {
             throw ApiException.wrap(e)
         }
     }
+}
+
+/**
+ * Creates a [MessageIn] with the payload already being serialized.
+ *
+ * @param payload Serialized message payload
+ * @param contentType Content type of the payload to send as a header. Defaults to `application/json`.
+ *
+ * See the class documentation for details about the other parameters.
+ */
+fun messageInRaw(
+    eventType: String,
+    payload: String,
+    contentType: String? = null,
+    application: ApplicationIn? = null,
+    channels: Set<String>? = null,
+    eventId: String? = null,
+    payloadRetentionHours: Long? = null,
+    payloadRetentionPeriod: Long? = 90L,
+    tags: Set<String>? = null,
+    transformationsParams: Map<String, Any> = mapOf(),
+): MessageIn {
+    val transformationsParams = transformationsParams.toMutableMap()
+    transformationsParams.put("rawPayload", payload)
+    if (contentType != null) {
+        val headers = mapOf("content-type" to contentType)
+        transformationsParams.put("headers", headers)
+    }
+
+    return MessageIn(
+        eventType = eventType,
+        payload = mapOf<String, String>(),
+        application = application,
+        channels = channels,
+        eventId = eventId,
+        payloadRetentionHours = payloadRetentionHours,
+        payloadRetentionPeriod = payloadRetentionPeriod,
+        tags = tags,
+        transformationsParams = transformationsParams,
+    )
 }
