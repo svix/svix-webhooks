5a4556ca2c2ec13bc2abdf7a4521c6eb72fc238c
Jonas Platte <jplatte@svix.com>
2025-01-10 16:34:10 +0100

    go: Pull in some generated code for message_attempt.go

--- go/message_attempt.go
+++ go/message_attempt.go
@@ -27,22 +27,33 @@ type MessageAttemptListOptions struct {
 }
 
 // Deprecated: use `ListByMsg` or `ListByEndpoint` instead
-func (m *MessageAttempt) List(
+func (messageAttempt *MessageAttempt) List(
 	ctx context.Context,
 	appId string,
 	msgId string,
 	options *MessageAttemptListOptions,
 ) (*ListResponseMessageAttemptOut, error) {
-	return m.ListByMsg(ctx, appId, msgId, options)
+	return messageAttempt.ListByMsg(ctx, appId, msgId, options)
 }
 
-func (m *MessageAttempt) ListByEndpoint(
+// List attempts by endpoint id
+//
+// Note that by default this endpoint is limited to retrieving 90 days' worth of data
+// relative to now or, if an iterator is provided, 90 days before/after the time indicated
+// by the iterator ID. If you require data beyond those time ranges, you will need to explicitly
+// set the `before` or `after` parameter as appropriate.
+func (messageAttempt *MessageAttempt) ListByEndpoint(
 	ctx context.Context,
 	appId string,
 	endpointId string,
 	options *MessageAttemptListOptions,
 ) (*ListResponseMessageAttemptOut, error) {
-	req := m.api.MessageAttemptAPI.V1MessageAttemptListByEndpoint(ctx, appId, endpointId)
+	req := messageAttempt.api.MessageAttemptAPI.V1MessageAttemptListByEndpoint(
+		ctx,
+		appId,
+		endpointId,
+	)
+
 	if options != nil {
 		if options.Iterator != nil {
 			req = req.Iterator(*options.Iterator)
@@ -78,20 +89,28 @@ func (m *MessageAttempt) ListByEndpoint(
 			req = req.Tag(*options.Tag)
 		}
 	}
+
 	ret, res, err := req.Execute()
 	if err != nil {
 		return nil, wrapError(err, res)
 	}
+
 	return ret, nil
 }
 
-func (m *MessageAttempt) ListByMsg(
+// List attempts by message ID.
+//
+// Note that by default this endpoint is limited to retrieving 90 days' worth of data
+// relative to now or, if an iterator is provided, 90 days before/after the time indicated
+// by the iterator ID. If you require data beyond those time ranges, you will need to explicitly
+// set the `before` or `after` parameter as appropriate.
+func (messageAttempt *MessageAttempt) ListByMsg(
 	ctx context.Context,
 	appId string,
 	msgId string,
 	options *MessageAttemptListOptions,
 ) (*ListResponseMessageAttemptOut, error) {
-	req := m.api.MessageAttemptAPI.V1MessageAttemptListByMsg(ctx, appId, msgId)
+	req := messageAttempt.api.MessageAttemptAPI.V1MessageAttemptListByMsg(ctx, appId, msgId)
 	if options != nil {
 		if options.Iterator != nil {
 			req = req.Iterator(*options.Iterator)
@@ -127,29 +146,50 @@ func (m *MessageAttempt) ListByMsg(
 			req = req.Tag(*options.Tag)
 		}
 	}
+
 	ret, res, err := req.Execute()
 	if err != nil {
 		return nil, wrapError(err, res)
 	}
+
 	return ret, nil
 }
 
-func (m *MessageAttempt) ListAttemptedMessages(
+// List messages for a particular endpoint. Additionally includes metadata about the latest message attempt.
+//
+// The `before` parameter lets you filter all items created before a certain date and is ignored if an iterator is passed.
+//
+// Note that by default this endpoint is limited to retrieving 90 days' worth of data
+// relative to now or, if an iterator is provided, 90 days before/after the time indicated
+// by the iterator ID. If you require data beyond those time ranges, you will need to explicitly
+// set the `before` or `after` parameter as appropriate.
+func (messageAttempt *MessageAttempt) ListAttemptedMessages(
 	ctx context.Context,
 	appId string,
 	endpointId string,
 	options *MessageAttemptListOptions,
 ) (*ListResponseEndpointMessageOut, error) {
-	req := m.api.MessageAttemptAPI.V1MessageAttemptListAttemptedMessages(ctx, appId, endpointId)
+	req := messageAttempt.api.MessageAttemptAPI.V1MessageAttemptListAttemptedMessages(
+		ctx,
+		appId,
+		endpointId,
+	)
+
 	if options != nil {
-		if options.Iterator != nil {
-			req = req.Iterator(*options.Iterator)
-		}
 		if options.Limit != nil {
 			req = req.Limit(*options.Limit)
 		}
+		if options.Iterator != nil {
+			req = req.Iterator(*options.Iterator)
+		}
+		if options.Channel != nil {
+			req = req.Channel(*options.Channel)
+		}
+		if options.Tag != nil {
+			req = req.Tag(*options.Tag)
+		}
 		if options.Status != nil {
-			req = req.Status(openapi.MessageStatus(*options.Status))
+			req = req.Status(*options.Status)
 		}
 		if options.Before != nil {
 			req = req.Before(*options.Before)
@@ -157,58 +197,76 @@ func (m *MessageAttempt) ListAttemptedMessages(
 		if options.After != nil {
 			req = req.After(*options.After)
 		}
-		if options.Channel != nil {
-			req = req.Channel(*options.Channel)
-		}
 		if options.WithContent != nil {
 			req = req.WithContent(*options.WithContent)
 		}
 		if options.EventTypes != nil {
 			req = req.EventTypes(*options.EventTypes)
 		}
-		if options.Tag != nil {
-			req = req.Tag(*options.Tag)
-		}
 	}
+
 	ret, res, err := req.Execute()
 	if err != nil {
 		return nil, wrapError(err, res)
 	}
+
 	return ret, nil
 }
 
-func (m *MessageAttempt) Get(
+// `msg_id`: Use a message id or a message `eventId`
+func (messageAttempt *MessageAttempt) Get(
 	ctx context.Context,
 	appId string,
 	msgId string,
-	attemptID string,
+	attemptId string,
 ) (*MessageAttemptOut, error) {
-	req := m.api.MessageAttemptAPI.V1MessageAttemptGet(ctx, appId, msgId, attemptID)
+	req := messageAttempt.api.MessageAttemptAPI.V1MessageAttemptGet(
+		ctx,
+		appId,
+		msgId,
+		attemptId,
+	)
+
 	ret, res, err := req.Execute()
 	if err != nil {
 		return nil, wrapError(err, res)
 	}
+
 	return ret, nil
 }
 
-func (m *MessageAttempt) ExpungeContent(
+// Deletes the given attempt's response body.
+//
+// Useful when an endpoint accidentally returned sensitive content.
+// The message can't be replayed or resent once its payload has been deleted or expired.
+func (messageAttempt *MessageAttempt) ExpungeContent(
 	ctx context.Context,
 	appId string,
 	msgId string,
 	attemptId string,
 ) error {
-	req := m.api.MessageAttemptAPI.V1MessageAttemptExpungeContent(ctx, appId, msgId, attemptId)
+	req := messageAttempt.api.MessageAttemptAPI.V1MessageAttemptExpungeContent(
+		ctx,
+		appId,
+		msgId,
+		attemptId,
+	)
+
 	res, err := req.Execute()
 	return wrapError(err, res)
 }
 
-func (m *MessageAttempt) ListAttemptedDestinations(
+// List endpoints attempted by a given message.
+//
+// Additionally includes metadata about the latest message attempt.
+// By default, endpoints are listed in ascending order by ID.
+func (messageAttempt *MessageAttempt) ListAttemptedDestinations(
 	ctx context.Context,
 	appId string,
 	msgId string,
 	options *MessageAttemptListOptions,
 ) (*ListResponseMessageEndpointOut, error) {
-	req := m.api.MessageAttemptAPI.V1MessageAttemptListAttemptedDestinations(ctx, appId, msgId)
+	req := messageAttempt.api.MessageAttemptAPI.V1MessageAttemptListAttemptedDestinations(ctx, appId, msgId)
 	if options != nil {
 		if options.Iterator != nil {
 			req = req.Iterator(*options.Iterator)
@@ -225,14 +283,14 @@ func (m *MessageAttempt) ListAttemptedDestinations(
 }
 
 // Deprecated: use `ListByMsg` instead, passing the endpoint ID through options
-func (m *MessageAttempt) ListAttemptsForEndpoint(
+func (messageAttempt *MessageAttempt) ListAttemptsForEndpoint(
 	ctx context.Context,
 	appId string,
 	msgId string,
 	endpointId string,
 	options *MessageAttemptListOptions,
 ) (*ListResponseMessageAttemptEndpointOut, error) {
-	req := m.api.MessageAttemptAPI.V1MessageAttemptListByEndpointDeprecated(ctx, appId, msgId, endpointId)
+	req := messageAttempt.api.MessageAttemptAPI.V1MessageAttemptListByEndpointDeprecated(ctx, appId, msgId, endpointId)
 	if options != nil {
 		if options.Iterator != nil {
 			req = req.Iterator(*options.Iterator)
@@ -263,31 +321,47 @@ func (m *MessageAttempt) ListAttemptsForEndpoint(
 	if err != nil {
 		return nil, wrapError(err, res)
 	}
+
 	return ret, nil
 }
 
-func (m *MessageAttempt) Resend(
+// Resend a message to the specified endpoint.
+func (messageAttempt *MessageAttempt) Resend(
 	ctx context.Context,
 	appId string,
 	msgId string,
 	endpointId string,
 ) error {
-	return m.ResendWithOptions(ctx, appId, msgId, endpointId, nil)
+	return messageAttempt.ResendWithOptions(
+		ctx,
+		appId,
+		msgId,
+		endpointId,
+		nil,
+	)
 }
 
-func (m *MessageAttempt) ResendWithOptions(
+// Resend a message to the specified endpoint.
+func (messageAttempt *MessageAttempt) ResendWithOptions(
 	ctx context.Context,
 	appId string,
 	msgId string,
 	endpointId string,
 	options *PostOptions,
 ) error {
-	req := m.api.MessageAttemptAPI.V1MessageAttemptResend(ctx, appId, msgId, endpointId)
+	req := messageAttempt.api.MessageAttemptAPI.V1MessageAttemptResend(
+		ctx,
+		appId,
+		msgId,
+		endpointId,
+	)
+
 	if options != nil {
 		if options.IdempotencyKey != nil {
 			req = req.IdempotencyKey(*options.IdempotencyKey)
 		}
 	}
+
 	res, err := req.Execute()
 	return wrapError(err, res)
 }
