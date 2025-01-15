5e755b982a928c0e41ae8b767c7609b07591b124
Jonas Platte <jplatte@svix.com>
2025-01-15 11:54:01 +0100

    Remove duplicate openapi.json file

--- server/svix-server/src/lib.rs
+++ server/svix-server/src/lib.rs
@@ -400,7 +400,7 @@ mod docs {
     }
 
     async fn get_openapi_json() -> impl IntoResponse {
-        static BODY: &str = include_str!("static/openapi.json");
+        static BODY: &str = include_str!("../../openapi.json");
         ([(http::header::CONTENT_TYPE, "application/json")], BODY)
     }
 }
