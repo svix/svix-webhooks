175c2941424bed4cd7eafefebb4e936ba2a52344
Mendy Man <mman@svix.com>
2025-01-16 14:21:14 -0500

    python: Add the authentication.py generated by the new openapi-codegen
    Note: the openapi.json file I am working with does not include some deprecated methods. For now I will manually add these deprecated calls by hand

--- python/svix/api/authentication.py
+++ python/svix/api/authentication.py
@@ -1,13 +1,43 @@
-from .common import PostOptions, ApiBase
+import typing as t
+from dataclasses import dataclass
+from deprecated import deprecated
+
+from .common import ApiBase, BaseOptions
+
 
 from ..internal.openapi_client.api.authentication import (
     v1_authentication_app_portal_access,
     v1_authentication_dashboard_access,
+    v1_authentication_expire_all,
     v1_authentication_logout,
 )
+
 from ..internal.openapi_client.models.app_portal_access_in import AppPortalAccessIn
 from ..internal.openapi_client.models.app_portal_access_out import AppPortalAccessOut
 from ..internal.openapi_client.models.dashboard_access_out import DashboardAccessOut
+from ..internal.openapi_client.models.application_token_expire_in import (
+    ApplicationTokenExpireIn,
+)
+
+
+@dataclass
+class AuthenticationAppPortalAccessOptions(BaseOptions):
+    idempotency_key: t.Optional[str] = None
+
+
+@dataclass
+class AuthenticationDashboardAccessOptions(BaseOptions):
+    idempotency_key: t.Optional[str] = None
+
+
+@dataclass
+class AuthenticationExpireAllOptions(BaseOptions):
+    idempotency_key: t.Optional[str] = None
+
+
+@dataclass
+class AuthenticationLogoutOptions(BaseOptions):
+    idempotency_key: t.Optional[str] = None
 
 
 class AuthenticationAsync(ApiBase):
@@ -15,8 +45,9 @@ class AuthenticationAsync(ApiBase):
         self,
         app_id: str,
         app_portal_access_in: AppPortalAccessIn,
-        options: PostOptions = PostOptions(),
+        options: AuthenticationAppPortalAccessOptions = AuthenticationAppPortalAccessOptions(),
     ) -> AppPortalAccessOut:
+        """Use this function to get magic links (and authentication codes) for connecting your users to the Consumer Application Portal."""
         return await v1_authentication_app_portal_access.request_asyncio(
             client=self._client,
             app_id=app_id,
@@ -24,14 +55,36 @@ class AuthenticationAsync(ApiBase):
             **options.to_dict(),
         )
 
+    @deprecated
     async def dashboard_access(
-        self, app_id: str, options: PostOptions = PostOptions()
+        self,
+        app_id: str,
+        options: AuthenticationDashboardAccessOptions = AuthenticationDashboardAccessOptions(),
     ) -> DashboardAccessOut:
         return await v1_authentication_dashboard_access.request_asyncio(
             client=self._client, app_id=app_id, **options.to_dict()
         )
 
-    async def logout(self, options: PostOptions = PostOptions()) -> None:
+    async def expire_all(
+        self,
+        app_id: str,
+        application_token_expire_in: ApplicationTokenExpireIn,
+        options: AuthenticationExpireAllOptions = AuthenticationExpireAllOptions(),
+    ) -> None:
+        """Expire all of the tokens associated with a specific application."""
+        return await v1_authentication_expire_all.request_asyncio(
+            client=self._client,
+            app_id=app_id,
+            json_body=application_token_expire_in,
+            **options.to_dict(),
+        )
+
+    async def logout(
+        self, options: AuthenticationLogoutOptions = AuthenticationLogoutOptions()
+    ) -> None:
+        """Logout an app token.
+
+        Trying to log out other tokens will fail."""
         return await v1_authentication_logout.request_asyncio(
             client=self._client, **options.to_dict()
         )
@@ -42,8 +95,9 @@ class Authentication(ApiBase):
         self,
         app_id: str,
         app_portal_access_in: AppPortalAccessIn,
-        options: PostOptions = PostOptions(),
+        options: AuthenticationAppPortalAccessOptions = AuthenticationAppPortalAccessOptions(),
     ) -> AppPortalAccessOut:
+        """Use this function to get magic links (and authentication codes) for connecting your users to the Consumer Application Portal."""
         return v1_authentication_app_portal_access.request_sync(
             client=self._client,
             app_id=app_id,
@@ -51,14 +105,36 @@ class Authentication(ApiBase):
             **options.to_dict(),
         )
 
+    def expire_all(
+        self,
+        app_id: str,
+        application_token_expire_in: ApplicationTokenExpireIn,
+        options: AuthenticationExpireAllOptions = AuthenticationExpireAllOptions(),
+    ) -> None:
+        """Expire all of the tokens associated with a specific application."""
+        return v1_authentication_expire_all.request_sync(
+            client=self._client,
+            app_id=app_id,
+            json_body=application_token_expire_in,
+            **options.to_dict(),
+        )
+
+    @deprecated
     def dashboard_access(
-        self, app_id: str, options: PostOptions = PostOptions()
+        self,
+        app_id: str,
+        options: AuthenticationDashboardAccessOptions = AuthenticationDashboardAccessOptions(),
     ) -> DashboardAccessOut:
         return v1_authentication_dashboard_access.request_sync(
             client=self._client, app_id=app_id, **options.to_dict()
         )
 
-    def logout(self, options: PostOptions = PostOptions()) -> None:
+    def logout(
+        self, options: AuthenticationLogoutOptions = AuthenticationLogoutOptions()
+    ) -> None:
+        """Logout an app token.
+
+        Trying to log out other tokens will fail."""
         return v1_authentication_logout.request_sync(
             client=self._client, **options.to_dict()
         )
