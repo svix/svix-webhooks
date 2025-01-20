cbd1f896f2d7dbd67c25981687f732e07334fa30
Mendy Man <mman@svix.com>
2025-01-20 17:06:12 -0500

    python: Add new low-level http client

--- python/svix/api/common.py
+++ python/svix/api/common.py
@@ -1,9 +1,16 @@
+import asyncio
+import random
+import time
 import typing as t
 from dataclasses import asdict, dataclass
 from datetime import datetime, timezone
+from http import HTTPStatus
 
+import httpx
 
 from ..internal.openapi_client.client import AuthenticatedClient
+from ..internal.openapi_client.errors import UnexpectedStatus
+from ..internal.openapi_client.models import HttpError, HTTPValidationError
 
 
 def ensure_tz(x: t.Optional[datetime]) -> t.Optional[datetime]:
@@ -44,3 +51,129 @@ class ApiBase:
 
     def __init__(self, client: AuthenticatedClient) -> None:
         self._client = client
+
+    def _get_httpx_kwargs(
+        self,
+        method: str,
+        path: str,
+        path_params: t.Optional[t.Dict[str, str]],
+        query_params: t.Optional[t.Dict[str, t.Optional[t.Any]]],
+        header_params: t.Optional[t.Dict[str, t.Optional[str]]],
+        json_body: t.Optional[t.Dict[str, t.Any]],
+    ) -> t.Dict[str, t.Any]:
+        if path_params is None:
+            url = f"{self._client.base_url}{path}"
+        else:
+            formatted_path = path.format(**path_params)
+            url = url = f"{self._client.base_url}{formatted_path}"
+
+        headers: t.Dict[str, str] = {
+            **self._client.get_headers(),
+            "svix-req-id": f"{random.getrandbits(32)}",
+        }
+        if header_params is not None:
+            headers.update({k: v for k, v in header_params.items() if v is not None})
+
+        httpx_kwargs = {
+            "method": method.upper(),
+            "url": url,
+            "headers": headers,
+            "cookies": self._client.get_cookies(),
+            "timeout": self._client.get_timeout(),
+            "follow_redirects": self._client.follow_redirects,
+            "verify": self._client.verify_ssl,
+        }
+
+        if query_params is not None:
+            httpx_kwargs["params"] = {
+                k: str(sanitize_field(v))
+                for k, v in query_params.items()
+                if v is not None
+            }
+
+        if json_body is not None:
+            httpx_kwargs["json"] = json_body
+        return httpx_kwargs
+
+    async def _execute_request_asyncio(
+        self,
+        method: str,
+        path: str,
+        path_params: t.Optional[t.Dict[str, str]] = None,
+        query_params: t.Optional[t.Dict[str, t.Optional[t.Any]]] = None,
+        header_params: t.Optional[t.Dict[str, t.Optional[str]]] = None,
+        json_body: t.Optional[t.Dict[str, t.Any]] = None,
+    ) -> httpx.Response:
+        httpx_kwargs = self._get_httpx_kwargs(
+            method,
+            path,
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            json_body=json_body,
+        )
+
+        response = httpx.request(**httpx_kwargs)
+
+        async with httpx.AsyncClient(verify=self._client.verify_ssl) as _client:
+            response = await _client.request(**httpx_kwargs)
+
+            for retry_count, sleep_time in enumerate(self._client.retry_schedule):
+                if response.status_code < 500:
+                    break
+
+                await asyncio.sleep(sleep_time)
+                httpx_kwargs["headers"]["svix-retry-count"] = str(retry_count)
+                response = await _client.request(**httpx_kwargs)
+
+        return _filter_response_for_errors_response(response)
+
+    def _execute_request_sync(
+        self,
+        method: str,
+        path: str,
+        path_params: t.Optional[t.Dict[str, str]] = None,
+        query_params: t.Optional[t.Dict[str, t.Optional[t.Any]]] = None,
+        header_params: t.Optional[t.Dict[str, t.Optional[str]]] = None,
+        json_body: t.Optional[t.Dict[str, t.Any]] = None,
+    ) -> httpx.Response:
+        httpx_kwargs = self._get_httpx_kwargs(
+            method,
+            path,
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            json_body=json_body,
+        )
+
+        response = httpx.request(**httpx_kwargs)
+        for retry_count, sleep_time in enumerate(self._client.retry_schedule):
+            if response.status_code < 500:
+                break
+
+            time.sleep(sleep_time)
+            httpx_kwargs["headers"]["svix-retry-count"] = str(retry_count)
+            response = httpx.request(**httpx_kwargs)
+
+        return _filter_response_for_errors_response(response)
+
+
+def _filter_response_for_errors_response(response: httpx.Response) -> httpx.Response:
+    if 200 <= response.status_code <= 299:
+        return response
+
+    if response.status_code == HTTPStatus.BAD_REQUEST:
+        raise HttpError.init_exception(response.json(), response.status_code)
+    if response.status_code == HTTPStatus.UNAUTHORIZED:
+        raise HttpError.init_exception(response.json(), response.status_code)
+    if response.status_code == HTTPStatus.FORBIDDEN:
+        raise HttpError.init_exception(response.json(), response.status_code)
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        raise HttpError.init_exception(response.json(), response.status_code)
+    if response.status_code == HTTPStatus.CONFLICT:
+        raise HttpError.init_exception(response.json(), response.status_code)
+    if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+        raise HTTPValidationError.init_exception(response.json(), response.status_code)
+    if response.status_code == HTTPStatus.TOO_MANY_REQUESTS:
+        raise HttpError.init_exception(response.json(), response.status_code)
+    raise UnexpectedStatus(response.status_code, response.content)
