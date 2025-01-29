c8c7c1e99059bcb80ba0666ddeff140cfbf85b15
Mendy Man <mman@svix.com>
2025-01-29 09:59:34 -0500

    csharp: Reorder+format inline with codegen

--- csharp/Svix/Application.cs
+++ csharp/Svix/Application.cs
@@ -24,34 +24,70 @@ namespace Svix
         public Application(ISvixClient svixClient, ApplicationApi applicationApi)
             : base(svixClient)
         {
-            _applicationApi = applicationApi ?? throw new ArgumentNullException(nameof(applicationApi));
+            _applicationApi =
+                applicationApi ?? throw new ArgumentNullException(nameof(applicationApi));
         }
 
-        public ApplicationOut Create(ApplicationIn application, ApplicationCreateOptions options = null, string idempotencyKey = default)
+        public async Task<ListResponseApplicationOut> ListAsync(
+            ListOptions options = null,
+            string idempotencyKey = default,
+            CancellationToken cancellationToken = default
+        )
         {
             try
             {
-                application = application ?? throw new ArgumentNullException(nameof(application));
+                var lResponse = await _applicationApi.V1ApplicationListAsync(
+                    options?.Limit,
+                    options?.Iterator,
+                    options?.Order,
+                    cancellationToken
+                );
 
-                var lApplication = _applicationApi.V1ApplicationCreate(
-                    application,
-                    options?.GetIfExists ?? false,
-                    idempotencyKey);
-
-                return lApplication;
+                return lResponse;
             }
             catch (ApiException e)
             {
-                Logger?.LogError(e, $"{nameof(Create)} failed");
+                Logger?.LogError(e, $"{nameof(ListAsync)} failed");
 
                 if (Throw)
                     throw;
 
-                return null;
+                return new ListResponseApplicationOut();
             }
         }
 
-        public async Task<ApplicationOut> CreateAsync(ApplicationIn application, ApplicationCreateOptions options = null, string idempotencyKey = default, CancellationToken cancellationToken = default)
+        public ListResponseApplicationOut List(
+            ListOptions options = null,
+            string idempotencyKey = default
+        )
+        {
+            try
+            {
+                var lResponse = _applicationApi.V1ApplicationList(
+                    options?.Limit,
+                    options?.Iterator,
+                    options?.Order
+                );
+
+                return lResponse;
+            }
+            catch (ApiException e)
+            {
+                Logger?.LogError(e, $"{nameof(List)} failed");
+
+                if (Throw)
+                    throw;
+
+                return new ListResponseApplicationOut();
+            }
+        }
+
+        public async Task<ApplicationOut> CreateAsync(
+            ApplicationIn application,
+            ApplicationCreateOptions options = null,
+            string idempotencyKey = default,
+            CancellationToken cancellationToken = default
+        )
         {
             try
             {
@@ -61,7 +97,8 @@ namespace Svix
                     application,
                     options?.GetIfExists ?? false,
                     idempotencyKey,
-                    cancellationToken);
+                    cancellationToken
+                );
 
                 return lApplication;
             }
@@ -76,43 +113,55 @@ namespace Svix
             }
         }
 
-        public bool Delete(string appId, string idempotencyKey = default)
+        public ApplicationOut Create(
+            ApplicationIn application,
+            ApplicationCreateOptions options = null,
+            string idempotencyKey = default
+        )
         {
             try
             {
-                var lResponse = _applicationApi.V1ApplicationDeleteWithHttpInfo(appId);
+                application = application ?? throw new ArgumentNullException(nameof(application));
 
-                return lResponse.StatusCode == HttpStatusCode.NoContent;
+                var lApplication = _applicationApi.V1ApplicationCreate(
+                    application,
+                    options?.GetIfExists ?? false,
+                    idempotencyKey
+                );
+
+                return lApplication;
             }
             catch (ApiException e)
             {
-                Logger?.LogError(e, $"{nameof(Delete)} failed");
+                Logger?.LogError(e, $"{nameof(Create)} failed");
 
                 if (Throw)
                     throw;
 
-                return false;
+                return null;
             }
         }
 
-        public async Task<bool> DeleteAsync(string appId, string idempotencyKey = default, CancellationToken cancellationToken = default)
+        public async Task<ApplicationOut> GetAsync(
+            string appId,
+            string idempotencyKey = default,
+            CancellationToken cancellationToken = default
+        )
         {
             try
             {
-                var lResponse = await _applicationApi.V1ApplicationDeleteWithHttpInfoAsync(
-                    appId,
-                    cancellationToken);
+                var lApplication = await _applicationApi.V1ApplicationGetAsync(appId);
 
-                return lResponse.StatusCode == HttpStatusCode.NoContent;
+                return lApplication;
             }
             catch (ApiException e)
             {
-                Logger?.LogError(e, $"{nameof(DeleteAsync)} failed");
+                Logger?.LogError(e, $"{nameof(GetAsync)} failed");
 
                 if (Throw)
                     throw;
 
-                return false;
+                return null;
             }
         }
 
@@ -135,99 +184,20 @@ namespace Svix
             }
         }
 
-        public async Task<ApplicationOut> GetAsync(string appId, string idempotencyKey = default, CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                var lApplication = await _applicationApi.V1ApplicationGetAsync(appId);
-
-                return lApplication;
-            }
-            catch (ApiException e)
-            {
-                Logger?.LogError(e, $"{nameof(GetAsync)} failed");
-
-                if (Throw)
-                    throw;
-
-                return null;
-            }
-        }
-
-        public ListResponseApplicationOut List(ListOptions options = null, string idempotencyKey = default)
-        {
-            try
-            {
-                var lResponse = _applicationApi.V1ApplicationList(
-                    options?.Limit,
-                    options?.Iterator,
-                    options?.Order);
-
-                return lResponse;
-            }
-            catch (ApiException e)
-            {
-                Logger?.LogError(e, $"{nameof(List)} failed");
-
-                if (Throw)
-                    throw;
-
-                return new ListResponseApplicationOut();
-            }
-        }
-
-        public async Task<ListResponseApplicationOut> ListAsync(ListOptions options = null, string idempotencyKey = default, CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                var lResponse = await _applicationApi.V1ApplicationListAsync(
-                    options?.Limit,
-                    options?.Iterator,
-                    options?.Order,
-                    cancellationToken);
-
-                return lResponse;
-            }
-            catch (ApiException e)
-            {
-                Logger?.LogError(e, $"{nameof(ListAsync)} failed");
-
-                if (Throw)
-                    throw;
-
-                return new ListResponseApplicationOut();
-            }
-        }
-
-        public ApplicationOut Update(string appId, ApplicationIn application, string idempotencyKey = default)
-        {
-            try
-            {
-                var lApplication = _applicationApi.V1ApplicationUpdate(
-                    appId,
-                    application);
-
-                return lApplication;
-            }
-            catch (ApiException e)
-            {
-                Logger?.LogError(e, $"{nameof(Update)} failed");
-
-                if (Throw)
-                    throw;
-
-                return null;
-            }
-        }
-
-        public async Task<ApplicationOut> UpdateAsync(string appId, ApplicationIn application, string idempotencyKey = default, CancellationToken cancellationToken = default)
+        public async Task<ApplicationOut> UpdateAsync(
+            string appId,
+            ApplicationIn application,
+            string idempotencyKey = default,
+            CancellationToken cancellationToken = default
+        )
         {
             try
             {
                 var lApplication = await _applicationApi.V1ApplicationUpdateAsync(
                     appId,
                     application,
-                    cancellationToken);
+                    cancellationToken
+                );
 
                 return lApplication;
             }
@@ -242,19 +212,21 @@ namespace Svix
             }
         }
 
-        public ApplicationOut Patch(string appId, ApplicationPatch application, string idempotencyKey = default)
+        public ApplicationOut Update(
+            string appId,
+            ApplicationIn application,
+            string idempotencyKey = default
+        )
         {
             try
             {
-                var lApplication = _applicationApi.V1ApplicationPatch(
-                    appId,
-                    application);
+                var lApplication = _applicationApi.V1ApplicationUpdate(appId, application);
 
                 return lApplication;
             }
             catch (ApiException e)
             {
-                Logger?.LogError(e, $"{nameof(Patch)} failed");
+                Logger?.LogError(e, $"{nameof(Update)} failed");
 
                 if (Throw)
                     throw;
@@ -263,20 +235,94 @@ namespace Svix
             }
         }
 
-        public async Task<ApplicationOut> PatchAsync(string appId, ApplicationPatch application, string idempotencyKey = default, CancellationToken cancellationToken = default)
+        public async Task<bool> DeleteAsync(
+            string appId,
+            string idempotencyKey = default,
+            CancellationToken cancellationToken = default
+        )
+        {
+            try
+            {
+                var lResponse = await _applicationApi.V1ApplicationDeleteWithHttpInfoAsync(
+                    appId,
+                    cancellationToken
+                );
+
+                return lResponse.StatusCode == HttpStatusCode.NoContent;
+            }
+            catch (ApiException e)
+            {
+                Logger?.LogError(e, $"{nameof(DeleteAsync)} failed");
+
+                if (Throw)
+                    throw;
+
+                return false;
+            }
+        }
+
+        public bool Delete(string appId, string idempotencyKey = default)
+        {
+            try
+            {
+                var lResponse = _applicationApi.V1ApplicationDeleteWithHttpInfo(appId);
+
+                return lResponse.StatusCode == HttpStatusCode.NoContent;
+            }
+            catch (ApiException e)
+            {
+                Logger?.LogError(e, $"{nameof(Delete)} failed");
+
+                if (Throw)
+                    throw;
+
+                return false;
+            }
+        }
+
+        public async Task<ApplicationOut> PatchAsync(
+            string appId,
+            ApplicationPatch application,
+            string idempotencyKey = default,
+            CancellationToken cancellationToken = default
+        )
         {
             try
             {
                 var lApplication = await _applicationApi.V1ApplicationPatchAsync(
                     appId,
                     application,
-                    cancellationToken);
+                    cancellationToken
+                );
 
                 return lApplication;
             }
             catch (ApiException e)
             {
                 Logger?.LogError(e, $"{nameof(PatchAsync)} failed");
+
+                if (Throw)
+                    throw;
+
+                return null;
+            }
+        }
+
+        public ApplicationOut Patch(
+            string appId,
+            ApplicationPatch application,
+            string idempotencyKey = default
+        )
+        {
+            try
+            {
+                var lApplication = _applicationApi.V1ApplicationPatch(appId, application);
+
+                return lApplication;
+            }
+            catch (ApiException e)
+            {
+                Logger?.LogError(e, $"{nameof(Patch)} failed");
 
                 if (Throw)
                     throw;
