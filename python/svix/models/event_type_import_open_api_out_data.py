0792e99b2404c97e2dfe46c569268b7a94a487f8
Mendy Man <mman@svix.com>
2025-01-30 17:19:26 -0500

    python: Add special case where the server does not send a camelCase field

--- python/svix/models/event_type_import_open_api_out_data.py
+++ python/svix/models/event_type_import_open_api_out_data.py
@@ -1,5 +1,6 @@
-# this file is @generated
 import typing as t
+
+from pydantic import Field
 
 from .common import BaseModel
 from .event_type_from_open_api import EventTypeFromOpenApi
@@ -8,4 +9,6 @@ from .event_type_from_open_api import EventTypeFromOpenApi
 class EventTypeImportOpenApiOutData(BaseModel):
     modified: t.List[str]
 
-    to_modify: t.Optional[t.List[EventTypeFromOpenApi]] = None
+    to_modify: t.Optional[t.List[EventTypeFromOpenApi]] = Field(
+        default=None, alias="to_modify"
+    )
