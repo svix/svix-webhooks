1efec91be3e70fca62b962e59f5f8a494d7a9b04
Mendy Man <mman@svix.com>
2025-01-30 16:16:34 -0500

    python: Add `populate_by_name` to model_config

--- python/svix/models/common.py
+++ python/svix/models/common.py
@@ -5,5 +5,6 @@ from pydantic.alias_generators import to_camel
 class BaseModel(pydantic.BaseModel):
     model_config = pydantic.ConfigDict(
         alias_generator=to_camel,
+        populate_by_name=True,
         json_encoders={set: lambda v: list(v)},
     )
