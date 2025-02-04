4206a1cdc0106292a1f72d09901f85f3683d6c88
Mendy Man <mman@svix.com>
2025-02-04 17:24:26 -0500

    python: Add HTTPValidationError

--- python/svix/exceptions.py
+++ python/svix/exceptions.py
@@ -1,7 +1,6 @@
-from .api.http_error import HttpError
+from .api.errors.http_error import HttpError
+from .api.errors.http_validation_error import HTTPValidationError
 from .webhooks import WebhookVerificationError
 
-__all__ = [
-    "HttpError",
-    "WebhookVerificationError",
-]
+
+__all__ = ["HttpError", "WebhookVerificationError", "HTTPValidationError"]
