92f8b21e8cf2f7dd7631c41450359406983303a1
Mendy Man <mman@svix.com>
2025-02-09 14:19:24 -0500

    go: Remove unused `wrapError` function

--- go/errors.go
+++ go/errors.go
@@ -1,11 +1,5 @@
 package svix
 
-import (
-	"net/http"
-
-	"github.com/svix/svix-webhooks/go/internal/openapi"
-)
-
 // Error provides access to the body, status, and error on returned errors.
 type Error struct {
 	status int
@@ -26,19 +20,4 @@ func (e Error) Body() []byte {
 // Status returns the HTTP status of the error.
 func (e Error) Status() int {
 	return e.status
-}
-
-// a simple function convert openapi errors to exposed svix.Error
-func wrapError(err error, res *http.Response) error {
-	if openapiError, ok := err.(*openapi.GenericOpenAPIError); ok {
-		e := &Error{
-			body:  openapiError.Body(),
-			error: openapiError.Error(),
-		}
-		if res != nil {
-			e.status = res.StatusCode
-		}
-		return e
-	}
-	return err
 }
