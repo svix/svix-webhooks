740c95b5d793c0893ea7339942ccbb527198d623
Mendy Man <mman@svix.com>
2025-02-09 17:20:46 -0500

    go: Add generic nullable type

---
+++ go/nullable.go
@@ -0,0 +1,35 @@
+package svix
+
+import "encoding/json"
+
+type Nullable[T any] struct {
+	val   T
+	isSet bool
+}
+
+func NewNullable[T any](value T) Nullable[T] {
+	return Nullable[T]{val: value, isSet: true}
+}
+
+func (n *Nullable[T]) UnmarshalJSON(data []byte) error {
+	n.isSet = true
+	if string(data) == "null" {
+		return nil
+	}
+
+	var value T
+	if err := json.Unmarshal(data, &value); err != nil {
+		return err
+	}
+
+	n.val = value
+	return nil
+}
+
+func (n Nullable[T]) MarshalJSON() ([]byte, error) {
+	if n.isSet {
+		return json.Marshal(n.val)
+	}
+	return nil, nil
+
+}
