3c57589e895e020323bea21c0ee8ea59335a26f5
Mendy Man <mman@svix.com>
2025-02-10 11:26:15 -0500

    go: Don't import these models into svix.go

--- go/svix.go
+++ go/svix.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/svix/svix-webhooks/go/internal/openapi"
 	"github.com/svix/svix-webhooks/go/internal/version"
 )
 
@@ -29,107 +28,7 @@ type (
 		Statistics                 *Statistics
 		OperationalWebhookEndpoint *OperationalWebhookEndpoint
 	}
-	AggregateEventTypesOut                    = openapi.AggregateEventTypesOut
-	ApplicationIn                             = openapi.ApplicationIn
-	ApplicationOut                            = openapi.ApplicationOut
-	ApplicationPatch                          = openapi.ApplicationPatch
-	ApplicationTokenExpireIn                  = openapi.ApplicationTokenExpireIn
-	AppPortalAccessIn                         = openapi.AppPortalAccessIn
-	AppPortalAccessOut                        = openapi.AppPortalAccessOut
-	AppUsageStatsIn                           = openapi.AppUsageStatsIn
-	AppUsageStatsOut                          = openapi.AppUsageStatsOut
-	BackgroundTaskOut                         = openapi.BackgroundTaskOut
-	BackgroundTaskStatus                      = openapi.BackgroundTaskStatus
-	BackgroundTaskType                        = openapi.BackgroundTaskType
-	DashboardAccessOut                        = openapi.DashboardAccessOut
-	EndpointHeadersIn                         = openapi.EndpointHeadersIn
-	EndpointHeadersOut                        = openapi.EndpointHeadersOut
-	EndpointHeadersPatchIn                    = openapi.EndpointHeadersPatchIn
-	EndpointIn                                = openapi.EndpointIn
-	EndpointMessageOut                        = openapi.EndpointMessageOut
-	EndpointOut                               = openapi.EndpointOut
-	EndpointPatch                             = openapi.EndpointPatch
-	EndpointSecretOut                         = openapi.EndpointSecretOut
-	EndpointSecretRotateIn                    = openapi.EndpointSecretRotateIn
-	EndpointStats                             = openapi.EndpointStats
-	EndpointTransformationIn                  = openapi.EndpointTransformationIn
-	EndpointTransformationOut                 = openapi.EndpointTransformationOut
-	EndpointUpdate                            = openapi.EndpointUpdate
-	EventExampleIn                            = openapi.EventExampleIn
-	EventTypeImportOpenApiIn                  = openapi.EventTypeImportOpenApiIn
-	EventTypeImportOpenApiOut                 = openapi.EventTypeImportOpenApiOut
-	EventTypeImportOpenApiOutData             = openapi.EventTypeImportOpenApiOutData
-	EventTypeIn                               = openapi.EventTypeIn
-	EventTypeOut                              = openapi.EventTypeOut
-	EventTypePatch                            = openapi.EventTypePatch
-	EventTypeUpdate                           = openapi.EventTypeUpdate
-	IntegrationIn                             = openapi.IntegrationIn
-	IntegrationKeyOut                         = openapi.IntegrationKeyOut
-	IntegrationOut                            = openapi.IntegrationOut
-	IntegrationUpdate                         = openapi.IntegrationUpdate
-	ListResponseApplicationOut                = openapi.ListResponseApplicationOut
-	ListResponseBackgroundTaskOut             = openapi.ListResponseBackgroundTaskOut
-	ListResponseEndpointMessageOut            = openapi.ListResponseEndpointMessageOut
-	ListResponseEndpointOut                   = openapi.ListResponseEndpointOut
-	ListResponseEventTypeOut                  = openapi.ListResponseEventTypeOut
-	ListResponseIntegrationOut                = openapi.ListResponseIntegrationOut
-	ListResponseMessageAttemptOut             = openapi.ListResponseMessageAttemptOut
-	ListResponseMessageEndpointOut            = openapi.ListResponseMessageEndpointOut
-	ListResponseMessageOut                    = openapi.ListResponseMessageOut
-	ListResponseOperationalWebhookEndpointOut = openapi.ListResponseOperationalWebhookEndpointOut
-	MessageAttemptEndpointOut                 = openapi.MessageAttemptEndpointOut
-	MessageAttemptOut                         = openapi.MessageAttemptOut
-	MessageEndpointOut                        = openapi.MessageEndpointOut
-	MessageIn                                 = openapi.MessageIn
-	MessageOut                                = openapi.MessageOut
-	MessageStatus                             = openapi.MessageStatus
-	OperationalWebhookEndpointIn              = openapi.OperationalWebhookEndpointIn
-	OperationalWebhookEndpointOut             = openapi.OperationalWebhookEndpointOut
-	OperationalWebhookEndpointSecretIn        = openapi.OperationalWebhookEndpointSecretIn
-	OperationalWebhookEndpointSecretOut       = openapi.OperationalWebhookEndpointSecretOut
-	OperationalWebhookEndpointUpdate          = openapi.OperationalWebhookEndpointUpdate
-	Ordering                                  = openapi.Ordering
-	RecoverIn                                 = openapi.RecoverIn
-	RecoverOut                                = openapi.RecoverOut
-	ReplayIn                                  = openapi.ReplayIn
-	ReplayOut                                 = openapi.ReplayOut
-	StatusCodeClass                           = openapi.StatusCodeClass
-	OperationalWebhookEndpointHeadersOut      = openapi.OperationalWebhookEndpointHeadersOut
-	OperationalWebhookEndpointHeadersIn       = openapi.OperationalWebhookEndpointHeadersIn
-
-	// Deprecated: Use EndpointGetStatsOptions
-	EndpointStatsOptions = EndpointGetStatsOptions
-	// Deprecated: Use ListResponseMessageAttemptOut
-	ListResponseMessageAttemptEndpointOut = openapi.ListResponseMessageAttemptOut
 )
-
-func String(s string) *string {
-	return &s
-}
-
-func NullableString(s *string) *openapi.NullableString {
-	return openapi.NewNullableString(s)
-}
-
-func StaticNullableString(s string) openapi.NullableString {
-	return *NullableString(String(s))
-}
-
-func NullableInt32(num *int32) *openapi.NullableInt32 {
-	return openapi.NewNullableInt32(num)
-}
-
-func NullableInt64(num *int64) *openapi.NullableInt64 {
-	return openapi.NewNullableInt64(num)
-}
-
-func Int32(i int32) *int32 {
-	return &i
-}
-
-func NullableBool(b *bool) *openapi.NullableBool {
-	return openapi.NewNullableBool(b)
-}
 
 func New(token string, options *SvixOptions) (*Svix, error) {
 	svixHttpClient := defaultSvixHttpClient()
