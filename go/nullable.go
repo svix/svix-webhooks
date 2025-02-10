84d510ce7b99fe1a361d50bc2246928511cbbbad
Mendy Man <mman@svix.com>
2025-02-10 11:24:14 -0500

    go: Move nullable to it's own package
    Since both svix and svix/models need to access nullable, I have to place it in a separate package to avoid circular imports

--- go/nullable.go
+++
@@ -1,7 +0,0 @@
-package svix
-
-import "github.com/svix/svix-webhooks/go/models"
-
-func NewNullable[T any](value T) models.Nullable[T] {
-	return models.NewNullable(value)
-}
---
+++ go/utils/nullable.go
@@ -0,0 +1,69 @@
+package utils
+
+import "encoding/json"
+
+type Nullable[T any] struct {
+	val   *T
+	isSet bool
+}
+
+// Create a new Nullable[T] that is explicitly set as null
+func NewNilNullable[T any]() Nullable[T] {
+	return Nullable[T]{val: nil, isSet: true}
+}
+
+// Create a new Nullable[T] that is unset, This type will not be emitted when serializing
+func NewNilUnsetNullable[T any]() Nullable[T] {
+	return Nullable[T]{val: nil, isSet: false}
+}
+
+func NewNullable[T any](value T) Nullable[T] {
+	return Nullable[T]{val: &value, isSet: true}
+}
+
+func (v Nullable[T]) Get() *T {
+	return v.val
+}
+
+func (v *Nullable[T]) Set(val *T) {
+	v.val = val
+	v.isSet = true
+}
+
+func (v Nullable[T]) IsSet() bool {
+	return v.isSet
+}
+
+func (v *Nullable[T]) Unset() {
+	v.val = nil
+	v.isSet = false
+}
+
+// Set the nullable value to nil, the when json serializing a null will be emitted
+func (v *Nullable[T]) SetNil() {
+	v.val = nil
+	v.isSet = true
+}
+
+func (n *Nullable[T]) UnmarshalJSON(data []byte) error {
+	n.isSet = true
+	if string(data) == "null" {
+		return nil
+	}
+
+	var value T
+	if err := json.Unmarshal(data, &value); err != nil {
+		return err
+	}
+
+	n.val = &value
+	return nil
+}
+
+func (n Nullable[T]) MarshalJSON() ([]byte, error) {
+	if n.isSet {
+		return json.Marshal(n.val)
+	}
+	return nil, nil
+
+}
