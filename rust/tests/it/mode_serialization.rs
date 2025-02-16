6ad6455e8dbef60834c6c3581437a9b1d6c6c280
Mendy Man <mman@svix.com>
2025-02-16 13:20:41 -0500

    rust: Fix this test

--- rust/tests/it/mode_serialization.rs
+++ rust/tests/it/mode_serialization.rs
@@ -17,7 +17,7 @@ async fn test_list_response_xxx_out() {
     assert_eq!(expected_model, loaded_json);
 
     // without iterator and prevIterator
-    let json_str = r#"{"data":[],"done":true}"#;
+    let json_str = r#"{"data":[],"done":true,"iterator":null,"prevIterator":null}"#;
     let loaded_json: ListResponseApplicationOut = serde_json::from_str(json_str).unwrap();
 
     let expected_model = ListResponseApplicationOut {
