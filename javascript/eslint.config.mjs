ff814bbefb115fd8b5515f5934db1d90842d6b3b
Mendy Man <mman@svix.com>
2025-02-17 11:41:28 -0500

    js: Migrate to eslint 9
    Also add dependency on mockttp

---
+++ javascript/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
