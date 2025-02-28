bc1dc2f27b89a4d9bb3ea0f63bf6f9d1d2f4bf1d
Owen Nelson <onelson@svix.com>
2025-02-27 17:01:37 -0800

    WIP: mark hidden=false for poller consumer, regen rust lib
    Ideally we'd be regenerating all the libs but not today. Today there are
    barriers that will interfere with the bridge work.

---
+++ rust/src/models/polling_endpoint_out.rs
@@ -0,0 +1,23 @@
+// this file is @generated
+use serde::{Deserialize, Serialize};
+
+use super::polling_endpoint_message_out::PollingEndpointMessageOut;
+
+#[derive(Clone, Debug, Default, PartialEq, Deserialize, Serialize)]
+pub struct PollingEndpointOut {
+    pub data: Vec<PollingEndpointMessageOut>,
+
+    pub done: bool,
+
+    pub iterator: String,
+}
+
+impl PollingEndpointOut {
+    pub fn new(data: Vec<PollingEndpointMessageOut>, done: bool, iterator: String) -> Self {
+        Self {
+            data,
+            done,
+            iterator,
+        }
+    }
+}
