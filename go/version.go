22ae5d28edc2efdd75381a2ccaa470221dd01ea7
Mendy Man <mman@svix.com>
2025-03-06 10:24:27 -0500

    Bump to version 1.61.3

--- go/version.go
+++ go/version.go
@@ -1,3 +1,3 @@
 package svix
 
-const Version = "1.61.2"
+const Version = "1.61.3"
--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -37,4 +37,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "1.61.2"
+__version__ = "1.61.3"
