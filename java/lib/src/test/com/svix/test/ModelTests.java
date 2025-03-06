ac01a5f0436e9c4b2b3732a34c7ed1ee5350aedf
Mendy Man <mman@svix.com>
2025-03-06 15:29:39 -0500

    java: Add tests for struct enum de/serialization

--- java/lib/src/test/com/svix/test/ModelTests.java
+++ java/lib/src/test/com/svix/test/ModelTests.java
@@ -3,7 +3,10 @@ package com.svix.test;
 import static org.junit.Assert.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.svix.models.CronConfig;
 import com.svix.models.DashboardAccessOut;
+import com.svix.models.IngestSourceIn;
+import com.svix.models.IngestSourceInConfig;
 
 import org.junit.Test;
 
@@ -19,5 +22,48 @@ public class ModelTests {
         DashboardAccessOut expectedModel = new DashboardAccessOut().token("asd");
         DashboardAccessOut model = DashboardAccessOut.fromJson("{\"token\":\"asd\"}");
         assertEquals(expectedModel, model);
+    }
+
+    @Test
+    public void structEnumWithNoExtraFields() throws JsonProcessingException {
+        String jsonString =
+                "{\"name\":\"mendy\",\"uid\":\"very"
+                        + " unique\",\"type\":\"generic-webhook\",\"config\":{}}";
+        IngestSourceIn sourceIn =
+                new IngestSourceIn(
+                        "mendy", "very unique", new IngestSourceInConfig.GenericWebhook());
+
+        assertEquals(jsonString, sourceIn.toJson());
+        assertEquals(IngestSourceIn.fromJson(jsonString), sourceIn);
+    }
+
+    @Test
+    public void structEnumWithFields() throws JsonProcessingException {
+        String jsonString =
+                "{\"name\":\"name\",\"uid\":\"uuiidd\",\"type\":\"cron\",\"config\":{\"contentType\":\"asd\",\"payload\":\"cool\",\"schedule\":\"*"
+                    + " * * * *\"}}";
+        IngestSourceIn sourceIn =
+                new IngestSourceIn(
+                        "name",
+                        "uuiidd",
+                        new IngestSourceInConfig.Cron(
+                                new CronConfig()
+                                        .contentType("asd")
+                                        .payload("cool")
+                                        .schedule("* * * * *")));
+        assertEquals(jsonString, sourceIn.toJson());
+        assertEquals(IngestSourceIn.fromJson(jsonString), sourceIn);
+    }
+
+    @Test
+    public void readStructEnumField() throws JsonProcessingException {
+        String jsonString =
+                "{\"name\":\"name\",\"uid\":\"uuiidd\",\"type\":\"cron\",\"config\":{\"contentType\":\"asd\",\"payload\":\"cool\",\"schedule\":\"*"
+                    + " * * * *\"}}";
+        IngestSourceIn sourceIn = IngestSourceIn.fromJson(jsonString);
+        assertTrue(sourceIn.getConfig() instanceof IngestSourceInConfig.Cron);
+        assertEquals(
+                "asd",
+                ((IngestSourceInConfig.Cron) sourceIn.getConfig()).getCron().getContentType());
     }
 }
