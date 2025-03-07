e59ebb8b4486155162b087536a29bc34e39a24c0
Mendy Man <mman@svix.com>
2025-03-07 10:45:47 -0500

    codegen: Bump image version

--- regen_openapi.py
+++ regen_openapi.py
@@ -15,7 +15,7 @@ except ImportError:
     print("Python 3.11 or greater is required to run the codegen")
     exit(1)
 
-OPENAPI_CODEGEN_IMAGE = "ghcr.io/svix/openapi-codegen:20250306-273"
+OPENAPI_CODEGEN_IMAGE = "ghcr.io/svix/openapi-codegen:20250307-275"
 REPO_ROOT = pathlib.Path(__file__).parent.resolve()
 DEBUG = os.getenv("DEBUG") is not None
 GREEN = "\033[92m"
