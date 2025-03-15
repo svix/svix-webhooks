6f72c33573a736c88e300ef223a4886ada1dc02f
Mendy Man <mman@svix.com>
2025-03-15 19:48:23 -0400

    python: Add struct enum tests

--- python/tests/test_httpretty.py
+++ python/tests/test_httpretty.py
@@ -1,5 +1,7 @@
 import svix
-from svix.api import MessageListOptions
+from svix.api import MessageListOptions,IngestSourceIn, CronConfig
+from svix.api.ingest_source import IngestSource
+
 import httpretty
 
 @httpretty.activate(verbose=True, allow_net_connect=False)
@@ -11,3 +13,41 @@ def test_octothorpe_in_query_param():
         body='{"data":[],"iterator":null,"prevIterator":null,"done":true}'
     )
     svx.message.list("app_id",MessageListOptions(tag="test#test"))
+
+@httpretty.activate(verbose=True, allow_net_connect=False)
+def test_struct_enum_with_fields():
+    svx = svix.Svix("token", svix.SvixOptions(server_url="http://test.example"))
+    ingest_source = IngestSource(svx._client)
+    httpretty.register_uri(
+        httpretty.POST,
+        "http://test.example/ingest/api/v1/source",
+        body='{"type":"cron","config":{"content_type":"mendy/tired","payload":"@hello there","schedule":"* * * * *"},"id":"src_2yZwUhtgs5Ai8T9yRQJXA","uid":"unique-identifier","name":"string","ingestUrl":"http://example.com","createdAt":"2019-08-24T14:15:22Z","updatedAt":"2019-08-24T14:15:22Z"}',
+    )
+    source_in = IngestSourceIn(
+        name="name",
+        type="cron",
+        config=CronConfig(content_type="asd", payload="asd", schedule="asd"),
+    )
+    res = ingest_source.create(source_in)
+    assert isinstance(res.config, CronConfig)
+
+    assert res.config.content_type == "mendy/tired"
+    assert res.config.payload == "@hello there"
+    assert res.config.schedule == "* * * * *"
+
+
+@httpretty.activate(verbose=True, allow_net_connect=False)
+def test_struct_enum_without_fields():
+    svx = svix.Svix("token", svix.SvixOptions(server_url="http://test.example"))
+    ingest_source = IngestSource(svx._client)
+    httpretty.register_uri(
+        httpretty.POST,
+        "http://test.example/ingest/api/v1/source",
+        body='{"type":"generic-webhook","config":{},"id":"src_2yZwUhtgs5Ai8T9yRQJXA","uid":"unique-identifier","name":"string","ingestUrl":"http://example.com","createdAt":"2019-08-24T14:15:22Z","updatedAt":"2019-08-24T14:15:22Z"}',
+    )
+    source_in = IngestSourceIn(name="name", type="generic-webhook", config={})
+
+    res = ingest_source.create(source_in)
+
+    assert isinstance(res.config, dict)
+    assert res.config == {}
