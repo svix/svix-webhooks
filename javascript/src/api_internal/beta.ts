ac412c9a6e7be54ba07c6b75d0171f2643cb3032
Jonas Platte <jplatte@svix.com>
2025-03-21 14:26:22 +0100

    js: Update api_resource template to support sub-resources

--- javascript/src/api_internal/beta.ts
+++ javascript/src/api_internal/beta.ts
@@ -1,5 +1,10 @@
 import { SvixRequestContext } from "../request";
+import { BetaConnector } from "./betaConnector";
 
 export class Beta {
   public constructor(private readonly requestCtx: SvixRequestContext) {}
+
+  public get connector() {
+    return new BetaConnector(this.requestCtx);
+  }
 }
