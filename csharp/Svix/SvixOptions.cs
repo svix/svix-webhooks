aaa0c515b1914b35e4f3dca5c9a5feb7d1e537af
Mendy Man <mman@svix.com>
2025-05-06 10:35:46 -0400

    csharp: Mark `BaseUrl` as deprecated  in favor of `ServerUrl` on `SvixOptions`

--- csharp/Svix/SvixOptions.cs
+++ csharp/Svix/SvixOptions.cs
@@ -2,17 +2,22 @@ namespace Svix
 {
     public class SvixOptions
     {
+        [Obsolete("BaseUrl is deprecated, please use ServerUrl instead.")]
         public string BaseUrl { get; }
+        public string ServerUrl { get; }
         public List<int> RetryScheduleMilliseconds { get; } = [50, 100, 200];
         public int TimeoutMilliseconds { get; } = 15000;
 
         public SvixOptions(
-            string baseUrl,
+            string serverUrl,
             int timeoutMilliseconds = 15000,
             List<int>? retryScheduleMilliseconds = null
         )
         {
-            BaseUrl = baseUrl;
+            ServerUrl = serverUrl;
+#pragma warning disable CS0618
+            BaseUrl = serverUrl;
+#pragma warning restore CS0618
             TimeoutMilliseconds = timeoutMilliseconds;
             retryScheduleMilliseconds ??= [50, 100, 200];
             if (retryScheduleMilliseconds.Count > 5)
