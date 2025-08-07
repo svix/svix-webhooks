2c49f895d7244c75617ea8d68e01ec03cdf23d45
Jonas Platte <jplatte@svix.com>
2025-08-07 19:34:51 +0200

    Publish version 1.71.0

--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -37,4 +37,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "1.70.1"
+__version__ = "1.71.0"
--- ruby/lib/svix/version.rb
+++ ruby/lib/svix/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Svix
-  VERSION = "1.70.1"
+  VERSION = "1.71.0"
 end
