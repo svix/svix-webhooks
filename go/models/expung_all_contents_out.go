6b375314d344b9f0d201ec9c2a63e76b28b42f5c
Mendy Man <mman@svix.com>
2025-08-11 17:45:11 -0400

    Remove the remaining misspelt `expung` files

--- go/models/expung_all_contents_out.go
+++
@@ -1,8 +0,0 @@
-// Package svix this file is @generated DO NOT EDIT
-package models
-
-type ExpungAllContentsOut struct {
-	Id     string               `json:"id"` // The QueueBackgroundTask's ID.
-	Status BackgroundTaskStatus `json:"status"`
-	Task   BackgroundTaskType   `json:"task"`
-}
