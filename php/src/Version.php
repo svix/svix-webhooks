fb6f7457ee7ee55996b6d07028bf28b5792266b8
Mendy <mman@svix.com>
2025-12-29 20:59:14 -0500

    Release v1.84.1 (#2151)
    Need to release again due to the JS release failure

--- php/src/Version.php
+++ php/src/Version.php
@@ -4,5 +4,5 @@ namespace Svix;
 
 class Version
 {
-    const VERSION = '1.83.0';
+    const VERSION = '1.84.1';
 }
--- python/svix/__init__.py
+++ python/svix/__init__.py
@@ -37,4 +37,4 @@ __all__ = [
     "WebhookVerificationError",
 ]
 
-__version__ = "1.84.0"
+__version__ = "1.84.1"
