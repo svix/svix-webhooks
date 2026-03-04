c5ca33e57be635a1a3e854effb4f883cbb0c34dc
Jonas Platte <jplatte@svix.com>
2026-03-04 17:58:32 +0100

    Upgrade opentelemetry libs to 0.29

--- bridge/svix-bridge/src/metrics.rs
+++ bridge/svix-bridge/src/metrics.rs
@@ -1,27 +1,15 @@
-use opentelemetry::metrics::{Meter, ObservableGauge};
-
-fn init_metric<T, E: std::fmt::Display>(result: Result<T, E>) -> Option<T> {
-    match result {
-        Ok(t) => Some(t),
-        Err(e) => {
-            tracing::error!("Failed to initialize metric: {}", e);
-            None
-        }
-    }
-}
+use opentelemetry::metrics::{Gauge, Meter};
 
 #[derive(Clone)]
 pub struct CommonMetrics {
-    mem_allocated_recorder: Option<ObservableGauge<u64>>,
-    mem_resident_recorder: Option<ObservableGauge<u64>>,
+    mem_allocated_recorder: Gauge<u64>,
+    mem_resident_recorder: Gauge<u64>,
 }
 
 impl CommonMetrics {
     pub fn new(meter: &Meter) -> Self {
-        let mem_resident_recorder =
-            init_metric(meter.u64_observable_gauge("svix.mem_resident").try_init());
-        let mem_allocated_recorder =
-            init_metric(meter.u64_observable_gauge("svix.mem_allocated").try_init());
+        let mem_resident_recorder = meter.u64_gauge("svix.mem_resident").build();
+        let mem_allocated_recorder = meter.u64_gauge("svix.mem_allocated").build();
 
         Self {
             mem_allocated_recorder,
@@ -30,14 +18,10 @@ impl CommonMetrics {
     }
 
     pub fn record_mem_allocated(&self, value: u64) {
-        if let Some(ref recorder) = self.mem_allocated_recorder {
-            recorder.observe(value, &[]);
-        }
+        self.mem_allocated_recorder.record(value, &[]);
     }
 
     pub fn record_mem_resident(&self, value: u64) {
-        if let Some(ref recorder) = self.mem_resident_recorder {
-            recorder.observe(value, &[]);
-        }
+        self.mem_resident_recorder.record(value, &[]);
     }
 }
