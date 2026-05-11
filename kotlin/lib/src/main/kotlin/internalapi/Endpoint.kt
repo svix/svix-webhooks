d9f56d9eddaf8ff355582f3d2a2c48bbb7ac93ac
svix-lucho <lucho@svix.com>
2026-05-11 17:27:42 -0300

    kotlin: start

---
+++ kotlin/lib/src/main/kotlin/internalapi/Endpoint.kt
@@ -0,0 +1,31 @@
+// this file is @generated
+package com.svix.kotlin.internal
+
+import com.svix.kotlin.models.EndpointTransformationIn
+
+class Endpoint(private val client: SvixHttpClient) {
+    val autoConfig: EndpointAutoConfig = EndpointAutoConfig(client)
+
+    /**
+     * This operation was renamed to `set-transformation`.
+     *
+     * @deprecated
+     */
+    @Deprecated("")
+    suspend fun transformationPartialUpdate(
+        appId: String,
+        endpointId: String,
+        endpointTransformationIn: EndpointTransformationIn,
+    ) {
+        val url =
+            client
+                .newUrlBuilder()
+                .encodedPath("/api/v1/app/$appId/endpoint/$endpointId/transformation")
+
+        client.executeRequest<EndpointTransformationIn, Boolean>(
+            "PATCH",
+            url.build(),
+            reqBody = endpointTransformationIn,
+        )
+    }
+}
