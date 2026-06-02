5d635aa1d9042c0e9791bca78cb9ef74b4baca9c
James Brown <jbrown@svix.com>
2026-06-02 02:58:00 -0700

    dotnet: include .NET runtime version in the user-agent for the csharp client library (#2367)

--- csharp/Svix.Tests/WiremockTests.cs
+++ csharp/Svix.Tests/WiremockTests.cs
@@ -165,6 +165,7 @@ namespace Svix.Tests
                 "svix-libs/",
                 stub.LogEntries[0].RequestMessage.Headers["User-Agent"][0]
             );
+            Assert.Contains("dotnet/v", stub.LogEntries[0].RequestMessage.Headers["User-Agent"][0]);
         }
 
         [Fact]
