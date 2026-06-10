76473b4a161c70c38628fed120d765f2211dc1a1
svix-lucho <lucho@svix.com>
2026-06-10 10:37:47 -0600

    cli: support using svix listen with an existing token

--- svix-cli/src/cmds/listen.rs
+++ svix-cli/src/cmds/listen.rs
@@ -1,12 +1,15 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::Args;
 
-use crate::config::{get_config_file_path, Config};
+use crate::config::Config;
 
 #[derive(Args)]
 pub struct ListenArgs {
     /// The local URL to forward webhooks to
     url: url::Url,
+    /// Connect to an existing Play token instead of creating a new one
+    #[arg(long)]
+    token: Option<String>,
     /// Disable TLS certificate verification when connecting to the local URL
     #[arg(long)]
     disable_tls_verification: bool,
@@ -14,26 +17,17 @@ pub struct ListenArgs {
 
 impl ListenArgs {
     pub async fn exec(self, cfg: &Config) -> Result<()> {
-        let token = match cfg.relay_token.as_ref() {
+        let (token, allow_token_rotation) = match self.token {
+            Some(token) => (token, false),
             None => {
                 let token = crate::relay::token::generate_token()?;
-                let mut updated_cfg = cfg.clone();
-                updated_cfg.relay_token = Some(token.clone());
-
-                let cfg_path = get_config_file_path()?;
-                if let Err(e) = updated_cfg.save_to_disk(&cfg_path).context(format!(
-                    "failed to save relay token to config file at `{}`",
-                    cfg_path.display()
-                )) {
-                    eprintln!("{e:#}");
-                }
-                token
+                (token, true)
             }
-            Some(token) => token.clone(),
         };
         crate::relay::listen(
             self.url,
             token,
+            allow_token_rotation,
             cfg.relay_debug_hostname.as_deref(),
             cfg.relay_disable_security.unwrap_or_default(),
             self.disable_tls_verification,
