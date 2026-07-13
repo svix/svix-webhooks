8e7691f3b4c5f58eca892f81664bd2b2f57c5d7b
Jonas Platte <jplatte@svix.com>
2026-07-13 17:34:18 +0200

    rust: Clean up non-generated model types

--- rust/src/models/validation_error.rs
+++ rust/src/models/validation_error.rs
@@ -1,9 +1,10 @@
 use serde::{Deserialize, Serialize};
 
 /// Validation errors have their own schema to provide context
-/// for invalid requests eg. mismatched types and out of bounds values. There
-/// may be any number of these per 422 UNPROCESSABLE ENTITY error.
-#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+/// for invalid requests eg. mismatched types and out of bounds values.
+///
+/// There may be any number of these per 422 UNPROCESSABLE ENTITY error.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ValidationError {
     /// The location as a [`Vec`] of [`String`]s -- often in the form
     /// `[\"body\", \"field_name\"]`, `[\"query\", \"field_name\"]`, etc. They
@@ -17,9 +18,6 @@ pub struct ValidationError {
 }
 
 impl ValidationError {
-    /// Validation errors have their own schema to provide context for invalid
-    /// requests eg. mismatched types and out of bounds values. There may be any
-    /// number of these per 422 UNPROCESSABLE ENTITY error.
     pub fn new(loc: Vec<String>, msg: String, r#type: String) -> ValidationError {
         ValidationError { loc, msg, r#type }
     }
