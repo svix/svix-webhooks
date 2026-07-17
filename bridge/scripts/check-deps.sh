669f7810afd8e1eead74db04d34843e2a794760a
James Brown <jbrown@svix.com>
2026-07-17 15:00:55 -0700

    docker: build CLI with alpine instead of debian

--- bridge/scripts/check-deps.sh
+++ bridge/scripts/check-deps.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+
+# this has to run under both bash on debian and busybox sh on alpine
 
 BINPATH="$1"
 
-if [[ -z "$BINPATH" ]]; then
+if [ -z "$BINPATH" ]; then
     echo >&2 "Usage $0 path/to/binary"
     exit 2
 fi
@@ -13,6 +15,6 @@ MISSING="$(ldd "$BINPATH" | grep 'not found')"
 # There'll be 1 linebreak for the empty case. Anything else and it means we
 # caught some unexpected output from ldd.
 if [ "1" != "$(echo "$MISSING" | wc -l)" ]; then
-    echo -e "Binary $BINPATH missing dependencies:\n$MISSING"
+    printf "Binary %s missing dependencies:\n%s" "$BINPATH" "$MISSING"
     exit 1
 fi
