ee0619fe3526861a2ba21b3f858d3efa8fd09997
Jonas Platte <jplatte@svix.com>
2026-07-28 11:58:33 +0200

    kotlin: Add missing APIs

--- kotlin/lib/src/main/kotlin/Svix.kt
+++ kotlin/lib/src/main/kotlin/Svix.kt
@@ -1,20 +1,24 @@
+// this file is @generated
 package com.svix.kotlin
 
-import com.svix.kotlin.SvixOptions
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
 class Svix(token: String, options: SvixOptions = SvixOptions()) {
     val application: Application
     val authentication: Authentication
+    val backgroundTask: BackgroundTask
+    val connector: Connector
     val endpoint: Endpoint
+    val environment: Environment
     val eventType: EventType
+    val health: Health
     val ingest: Ingest
     val integration: Integration
     val message: Message
     val messageAttempt: MessageAttempt
+    val operationalWebhook: OperationalWebhook
     val statistics: Statistics
     val streaming: Streaming
-    val operationalWebhook: OperationalWebhook
 
     init {
         val baseUrl = options.baseUrl ?: baseUrlFromToken(token)
@@ -23,14 +27,18 @@ class Svix(token: String, options: SvixOptions = SvixOptions()) {
 
         application = Application(httpClient)
         authentication = Authentication(httpClient)
+        backgroundTask = BackgroundTask(httpClient)
+        connector = Connector(httpClient)
         endpoint = Endpoint(httpClient)
+        environment = Environment(httpClient)
         eventType = EventType(httpClient)
+        health = Health(httpClient)
         ingest = Ingest(httpClient)
         integration = Integration(httpClient)
         message = Message(httpClient)
         messageAttempt = MessageAttempt(httpClient)
+        operationalWebhook = OperationalWebhook(httpClient)
         statistics = Statistics(httpClient)
         streaming = Streaming(httpClient)
-        operationalWebhook = OperationalWebhook(httpClient)
     }
 }
