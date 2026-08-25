da9b423d505f27206668db219de72cb6d69f894a
Jonas Platte <jplatte@svix.com>
2026-08-25 14:05:48 +0200

    Release SDKs + CLI v2.1.0, for real (#2578)

--- csharp/Svix/Version.cs
+++ csharp/Svix/Version.cs
@@ -2,6 +2,6 @@ namespace Svix
 {
     public static class Version
     {
-        public const string version = "2.0.0";
+        public const string version = "2.1.0";
     }
 }
--- php/src/Version.php
+++ php/src/Version.php
@@ -4,5 +4,5 @@ namespace Svix;
 
 class Version
 {
-    public const VERSION = '2.0.0';
+    public const VERSION = '2.1.0';
 }
