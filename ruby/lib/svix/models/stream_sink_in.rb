937bb1e696a100473f1668cbe5c233d6ad4ba3ba
Anish Kr Singh <59671878+anishfyi@users.noreply.github.com>
2026-09-01 04:22:26 +0530

    libs/ruby: support nested model JSON generation (#2607)
    Fixes #2598.
    
    Generated Ruby models define `to_json` without accepting JSON generator
    state. `JSON.generate` passes this state when a model is nested in an
    array or object, causing an argument-count error.

--- ruby/lib/svix/models/stream_sink_in.rb
+++ ruby/lib/svix/models/stream_sink_in.rb
@@ -31,8 +31,8 @@ module Svix
       end
       # Serializes the object to a json string
       # @return String
-      def to_json
-        JSON.dump(serialize)
+      def to_json(*args)
+        serialize.to_json(*args)
       end
     end
 
@@ -193,8 +193,8 @@ module Svix
 
     # Serializes the object to a json string
     # @return String
-    def to_json
-      JSON.dump(serialize)
+    def to_json(*args)
+      serialize.to_json(*args)
     end
 
   end
