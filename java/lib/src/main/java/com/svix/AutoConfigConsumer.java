bbd17a6d3aaf01c0693624880c88c17dcb96f933
svix-lucho <lucho@svix.com>
2026-09-01 14:29:33 -0300

    java: support v1 and v2 autoconfig tokens

--- java/lib/src/main/java/com/svix/AutoConfigConsumer.java
+++ java/lib/src/main/java/com/svix/AutoConfigConsumer.java
@@ -2,21 +2,29 @@ package com.svix;
 
 import com.svix.exceptions.ApiException;
 import com.svix.internalapi.EndpointAutoConfigDeprecated;
+import com.svix.internalapi.DestinationAutoconfig;
 import com.svix.internalapi.MessagePollerv2;
 import com.svix.internalapi.MessagePollerv2ConsumerCommitOptions;
 import com.svix.internalapi.MessagePollerv2ConsumerPollOptions;
 import com.svix.models.AutoConfigSinkType;
 import com.svix.models.AutoConfigSinkTypeConfig;
+import com.svix.models.DestinationIn;
+import com.svix.models.DestinationInConfig;
+import com.svix.models.DestinationOut;
+import com.svix.models.DestinationOutConfig;
 import com.svix.models.EndpointOut;
 import com.svix.models.PollerV2CommitIn;
 import com.svix.models.PollerV2PollOut;
 import com.svix.models.SinkInCommon;
+import com.svix.models.SinkStatus;
 import com.svix.models.SubscribeIn;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public final class AutoConfigConsumer {
   private final String appId;
-  private final String sinkId;
+  private String sinkId;
+  private final String autoconfigId;
   private final SinkInCommon sinkIn;
   private final Svix svix;
 
@@ -25,6 +33,7 @@ public final class AutoConfigConsumer {
     AutoConfig.DecodedTokenContent content = AutoConfig.decodeToken(token);
     this.appId = content.getAppId();
     this.sinkId = content.getEndpointId();
+    this.autoconfigId = content.getAutoconfigId();
     this.sinkIn = sinkIn;
 
     try {
@@ -37,13 +46,26 @@ public final class AutoConfigConsumer {
   }
 
   /** Registers this polling sink with Svix using the auto-config API. */
-  public EndpointOut subscribe() throws IOException, ApiException {
-    return new EndpointAutoConfigDeprecated(svix.getHttpClient()).update(appId, sinkId, new SubscribeIn()
-        .sink(new AutoConfigSinkType(new AutoConfigSinkTypeConfig.Poller(sinkIn))));
+  public DestinationOut subscribe() throws IOException, ApiException {
+    if (autoconfigId != null) {
+      DestinationOut destination = new DestinationAutoconfig(svix.getHttpClient())
+          .subscribe(appId, autoconfigId, sinkInCommonToPollingDestination(sinkIn));
+      this.sinkId = destination.getId();
+      return destination;
+    }
+
+    EndpointOut endpoint =
+        new EndpointAutoConfigDeprecated(svix.getHttpClient()).update(appId, sinkId, new SubscribeIn()
+            .sink(new AutoConfigSinkType(
+                new AutoConfigSinkTypeConfig.Poller(sinkIn))));
+    return destinationOutFromV1Endpoint(endpoint);
   }
 
   public PollerV2PollOut receive(final String consumerId,
       final MessagePollerv2ConsumerPollOptions options) throws IOException, ApiException {
+    if (sinkId == null) {
+      sinkId = subscribe().getId();
+    }
     return new MessagePollerv2(svix.getHttpClient()).consumerPoll(appId, sinkId, consumerId,
         options);
   }
@@ -54,6 +76,9 @@ public final class AutoConfigConsumer {
 
   public void commit(final String consumerId, final long offset,
       final MessagePollerv2ConsumerCommitOptions options) throws IOException, ApiException {
+    if (sinkId == null) {
+      sinkId = subscribe().getId();
+    }
     PollerV2CommitIn commitIn = new PollerV2CommitIn();
     commitIn.setOffset(offset);
     new MessagePollerv2(svix.getHttpClient()).consumerCommit(appId, sinkId, consumerId, commitIn,
@@ -62,5 +87,38 @@ public final class AutoConfigConsumer {
 
   public void commit(final String consumerId, final long offset) throws IOException, ApiException {
     commit(consumerId, offset, new MessagePollerv2ConsumerCommitOptions());
+  }
+
+  private static DestinationOut destinationOutFromV1Endpoint(final EndpointOut endpoint) {
+    DestinationOut destination = new DestinationOut();
+    destination.setId(endpoint.getId());
+    destination.setUid(endpoint.getUid());
+    destination.setStatus(
+        Boolean.TRUE.equals(endpoint.getDisabled()) ? SinkStatus.DISABLED : SinkStatus.ENABLED);
+    destination.setCreatedAt(endpoint.getCreatedAt());
+    destination.setUpdatedAt(endpoint.getUpdatedAt());
+    if (endpoint.getEventTypes() != null) {
+      destination.setEventTypes(new ArrayList<>(endpoint.getEventTypes()));
+    }
+    if (endpoint.getChannels() != null) {
+      destination.setChannels(new ArrayList<>(endpoint.getChannels()));
+    }
+    destination.setMetadata(endpoint.getMetadata());
+    destination.setConfig(new DestinationOutConfig.PollingEndpoint());
+    return destination;
+  }
+
+  private static DestinationIn sinkInCommonToPollingDestination(final SinkInCommon sink) {
+    DestinationIn destination = new DestinationIn()
+        .config(new DestinationInConfig.PollingEndpoint())
+        .uid(sink.getUid())
+        .metadata(sink.getMetadata());
+    if (sink.getEventTypes() != null) {
+      destination.eventTypes(new ArrayList<>(sink.getEventTypes()));
+    }
+    if (sink.getChannels() != null) {
+      destination.channels(new ArrayList<>(sink.getChannels()));
+    }
+    return destination;
   }
 }
