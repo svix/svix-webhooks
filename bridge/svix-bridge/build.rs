2db80ff87265adf23682c192960c1f429f1c02b6
Jonas Platte <jplatte@svix.com>
2026-09-08 18:39:09 +0200

    Only write executable metadata in Docker builds (#2627)

--- bridge/svix-bridge/build.rs
+++ bridge/svix-bridge/build.rs
@@ -1,18 +1,19 @@
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-
-    let mut package_metadata =
-        rust_executable_metadata::PackageMetadataBuilder::new_from_cargo("svix")
+    println!("cargo:rerun-if-env-changed=WRITE_EXECUTABLE_METADATA");
+    if std::env::var("WRITE_EXECUTABLE_METADATA").is_ok_and(|v| v == "1") {
+        let mut package_metadata =
+            rust_executable_metadata::PackageMetadataBuilder::new_from_cargo("svix")
+                .unwrap()
+                .license("MIT")
+                .maintainer("support@svix.com")
+                .copyright("Svix Inc");
+        if let Ok(sha) = std::env::var("GITHUB_SHA") {
+            package_metadata = package_metadata.hash(sha);
+        }
+        package_metadata
+            .build()
             .unwrap()
-            .license("MIT")
-            .maintainer("support@svix.com")
-            .copyright("Svix Inc");
-    if let Ok(sha) = std::env::var("GITHUB_SHA") {
-        package_metadata = package_metadata.hash(sha);
+            .write_linker_script_and_inject_argument()
+            .unwrap();
     }
-    package_metadata
-        .build()
-        .unwrap()
-        .write_linker_script_and_inject_argument()
-        .unwrap();
 }
