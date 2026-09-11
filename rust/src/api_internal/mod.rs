b476b8181ad836a61dc3067c9fae231a800c8bb1
svix-lucho <lucho@svix.com>
2026-09-11 12:15:43 -0300

    Remove leftover stuff

--- rust/src/api_internal/mod.rs
+++ rust/src/api_internal/mod.rs
@@ -1,14 +1,7 @@
-#[allow(dead_code)]
 pub mod autoconfig_subscription;
-#[allow(dead_code)]
 pub mod autoconfig_subscription_destination;
-#[allow(dead_code)]
 pub mod autoconfig_subscription_endpoint;
-#[allow(dead_code)]
-mod endpoint;
 pub mod endpoint_auto_config_deprecated;
-#[allow(dead_code)]
-mod message;
 pub mod message_pollerv2;
 
 use crate::Configuration;
@@ -18,7 +11,6 @@ use autoconfig_subscription_endpoint::AutoconfigSubscriptionEndpoint;
 use endpoint_auto_config_deprecated::EndpointAutoConfigDeprecated;
 use message_pollerv2::MessagePollerv2;
 
-#[allow(dead_code)]
 pub(crate) fn autoconfig_subscription(cfg: &Configuration) -> AutoconfigSubscription<'_> {
     AutoconfigSubscription::new(cfg)
 }
