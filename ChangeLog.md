4e8146fe22fc1907623d213baf04263e62b8fbfc
Luciano Bianchi <133019767+svix-lucho@users.noreply.github.com>
2026-09-17 20:09:34 -0300

    PHP: Add AutoConfigConsumer (#2649)

--- ChangeLog.md
+++ ChangeLog.md
@@ -6,7 +6,8 @@ The Svix Server changelog has moved to [server/ChangeLog.md](./server/ChangeLog.
 The Svix Bridge changelog has moved to [bridge/ChangeLog.md](./bridge/ChangeLog.md).
 
 ## Unreleased
-*
+* Libs/PHP: Add support for `destination` and `stream` APIs
+* Libs/PHP: Add `AutoConfigConsumer`
 
 ## Version 2.5.0
 * Libs **(New)**: Add `autoconfig` API support to create and rotate AutoConfig tokens
---
+++ php/src/ApiInternal/MessagePollerv2ConsumerPollOptions.php
@@ -0,0 +1,18 @@
+<?php
+
+// this file is @generated
+declare(strict_types=1);
+
+namespace Svix\ApiInternal;
+
+use Svix\Models\StartingPosition;
+
+class MessagePollerv2ConsumerPollOptions
+{
+    public function __construct(
+        public readonly ?int $limit = null,
+        public readonly ?int $leaseDurationMs = null,
+        public readonly ?StartingPosition $startingPosition = null,
+    ) {
+    }
+}
